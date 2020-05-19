fcddb8a225db8315276032482bdcf4d75f4b4ff7
戴凯杰 <daikaijie@bjknrt.com>
2020-05-19 14:23:05 +0800

    格式化文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,54 @@
 module.exports = {
   root: true,
   globals: {
-    _DEV_: 'readonly',
+    _DEV_: 'readonly'
   },
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'error'
   },
   overrides: [
     {
       files: ['*.ts'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.json'
       },
       extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
+        'plugin:prettier/recommended'
       ],
       plugins: ['@typescript-eslint', 'prettier'],
       rules: {
-        'prettier/prettier': 'error',
-      },
+        'prettier/prettier': 'error'
+      }
     },
     {
       files: ['*.js'],
       parser: 'babel-eslint',
       extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-      plugins: ['prettier'],
+      plugins: ['prettier']
     },
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: 'babel-eslint',
-        sourceType: 'module',
+        sourceType: 'module'
       },
       extends: [
         'eslint:recommended',
         'plugin:vue/recommended',
         'plugin:prettier/recommended',
-        'prettier/vue',
+        'prettier/vue'
       ],
-      plugins: ['vue', 'prettier'],
-    },
-  ],
+      plugins: ['vue', 'prettier']
+    }
+  ]
 };
