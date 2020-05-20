81d86397748c787226a41cb40f0e0ed0a7d0f096
吴成浩 <wuchenghao@bjknrt.com>
2020-05-20 15:33:03 +0800

    添加permission表达文件

---
+++ common/permission.ts
@@ -0,0 +1,30 @@
+export const Permission = [
+  {
+    key: 'home',
+    description: '首页'
+  },
+  {
+    key: 'user',
+    group: 'user',
+    description: '用户管理'
+  },
+  {
+    key: 'user-add',
+    group: 'user',
+    description: '用户添加'
+  },
+  {
+    key: 'user-update',
+    group: 'user',
+    description: '用户更新'
+  },
+  {
+    key: 'user-remove',
+    group: 'user',
+    description: '用户删除'
+  },
+  {
+    key: 'role',
+    description: '角色管理'
+  }
+];
