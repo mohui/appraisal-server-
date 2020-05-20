33ed3e1c216fb30624e60102b07c0d0b5343a46c
吴成浩 <wuchenghao@bjknrt.com>
2020-05-20 16:32:48 +0800

    postgresDB修改为appraisalDB

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ export class Application {
   server = http.createServer(this.express);
   dataDB = new MySQL(config.get('data'));
   knrtDB = new MySQL(config.get('knrt'));
-  postgresDB = createExtendedSequelize(
+  appraisalDB = createExtendedSequelize(
     new Sequelize({
       dialect: 'postgres',
       host: config.get('postgres.host'),
@@ -84,7 +84,7 @@ export class Application {
 
   async shutdown() {
     //关闭数据库
-    this.postgresDB && (await this.postgresDB.close());
+    this.appraisalDB && (await this.appraisalDB.close());
     //关闭http服务器
     return new Promise(resolve => {
       this.server[(this.server as any).kill ? 'kill' : 'close'](() =>
@@ -94,8 +94,8 @@ export class Application {
   }
 
   async initDB() {
-    this.postgresDB.addModels(Object.values(models));
-    const migrate = new Migrater(this.postgresDB);
+    this.appraisalDB.addModels(Object.values(models));
+    const migrate = new Migrater(this.appraisalDB);
     migrations.forEach(m => migrate.addMigration(m));
     await migrate.migrate(1);
   }
@@ -154,4 +154,4 @@ export const app = new Application();
 //导出各种便捷属性
 export const dataDB = app.dataDB;
 export const knrtDB = app.knrtDB;
-export const postgresDB = app.postgresDB;
+export const appraisalDB = app.appraisalDB;
