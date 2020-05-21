7ee1a185dfc484644b11439f9aedeb9ccbedb90c
吴成浩 <wuchenghao@bjknrt.com>
2020-05-21 16:55:57 +0800

    完善权限表达文件

--- common/permission.ts
+++ common/permission.ts
@@ -1,30 +1,32 @@
-export const Permission = [
-  {
+export const Permission = {
+  HOME: {
     key: 'home',
     name: '首页'
   },
-  {
-    key: 'user',
+  USER_INDEX: {
+    key: 'user-index',
     group: 'user',
-    name: '用户管理'
+    name: '用户首页'
   },
-  {
+  USER_ADD: {
     key: 'user-add',
     group: 'user',
     name: '用户添加'
   },
-  {
+  USER_UPDATE: {
     key: 'user-update',
     group: 'user',
     name: '用户更新'
   },
-  {
+  USER_REMOVE: {
     key: 'user-remove',
     group: 'user',
     name: '用户删除'
   },
-  {
-    key: 'role',
+  ROLE_INDEX: {
+    key: 'role-index',
     name: '角色管理'
   }
-];
+};
+
+export const PermissionArray = Object.values(Permission);
