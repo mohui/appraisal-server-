c74e3b2e011dc02de9ed1f100d4ef6bd5338886a
吴成浩 <wuchenghao@bjknrt.com>
2020-05-22 10:21:46 +0800

    导出权限的映射关系

--- common/permission.ts
+++ common/permission.ts
@@ -1,32 +1,35 @@
-export const Permission = {
-  HOME: {
+export const Permission = [
+  {
     key: 'home',
     name: '首页'
   },
-  USER_INDEX: {
+  {
     key: 'user-index',
-    group: 'user',
     name: '用户首页'
   },
-  USER_ADD: {
+  {
     key: 'user-add',
-    group: 'user',
     name: '用户添加'
   },
-  USER_UPDATE: {
+  {
     key: 'user-update',
-    group: 'user',
     name: '用户更新'
   },
-  USER_REMOVE: {
+  {
     key: 'user-remove',
-    group: 'user',
     name: '用户删除'
   },
-  ROLE_INDEX: {
+  {
     key: 'role-index',
-    name: '角色管理'
+    name: '角色首页'
   }
-};
+];
 
-export const PermissionArray = Object.values(Permission);
+export const PermissionMapping = {
+  HOME: 'home',
+  USER_INDEX: 'user-index',
+  USER_ADD: 'user-add',
+  USER_UPDATE: 'user-update',
+  USER_REMOVE: 'user-remove',
+  ROLE_INDEX: 'role-index'
+};
