84b7c0d3cc25a2267f278b0a5b05210197cee8b1
吴成浩 <wuchenghao@bjknrt.com>
2020-05-23 19:08:33 +0800

    规则表的初始化脚本

--- src/database/migrations/002_check_system.ts
+++ src/database/migrations/002_check_system.ts
@@ -42,12 +42,47 @@ export class CheckSystemMigration implements IMigration {
       COMMENT ON COLUMN "check_system"."check_year" IS '考核年度';
       COMMENT ON COLUMN "check_system"."status" IS '状态';
       COMMENT ON COLUMN "check_system"."remarks" IS '备注';
+
+      CREATE TABLE IF NOT EXISTS "check_rule"
+      (
+        "rule_id"           UUID,
+        "check_id"          VARCHAR(50),
+        "parent_rule_id"    VARCHAR(50),
+        "rule_name"         VARCHAR(255),
+        "rule_score"        INTEGER,
+        "check_standard"    VARCHAR(50),
+        "check_method"      VARCHAR(50),
+        "evaluate_standard" VARCHAR(50),
+        "create_by"         VARCHAR(50),
+        "update_by"         VARCHAR(50),
+        "status"            VARCHAR(50),
+        "standard_ids"      VARCHAR(255)[],
+        "standard_names"    VARCHAR(255)[] ,
+        "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+        "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+        "deleted_at" TIMESTAMP WITH TIME ZONE,
+        PRIMARY KEY ("rule_id")
+      );
+      COMMENT ON COLUMN "check_rule"."rule_id" IS '规则主键id';
+      COMMENT ON COLUMN "check_rule"."check_id" IS '考核体系id';
+      COMMENT ON COLUMN "check_rule"."parent_rule_id" IS '父规则id';
+      COMMENT ON COLUMN "check_rule"."rule_name" IS '规则名称';
+      COMMENT ON COLUMN "check_rule"."rule_score" IS '得分';
+      COMMENT ON COLUMN "check_rule"."check_standard" IS '考核标准';
+      COMMENT ON COLUMN "check_rule"."check_method" IS '考核方法';
+      COMMENT ON COLUMN "check_rule"."evaluate_standard" IS '评分标准';
+      COMMENT ON COLUMN "check_rule"."create_by" IS '创建人';
+      COMMENT ON COLUMN "check_rule"."update_by" IS '修改人';
+      COMMENT ON COLUMN "check_rule"."status" IS '状态';
+      COMMENT ON COLUMN "check_rule"."standard_ids" IS '关联指标Id';
+      COMMENT ON COLUMN "check_rule"."standard_names" IS '关联的指标名称';
     `);
   }
 
   async down(client: ExtendedSequelize): Promise<void> {
     await client.execute(`
       DROP TABLE IF EXISTS "check_system";
+      DROP TABLE IF EXISTS "check_rule";
     `);
   }
 }
