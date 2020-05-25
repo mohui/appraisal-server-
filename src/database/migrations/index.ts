0b7fe048f138bd3de4d331a483f0131d434219d4
吴成浩 <wuchenghao@bjknrt.com>
2020-05-25 16:14:12 +0800

    行政机构表的迁移脚本

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -1,9 +1,11 @@
 import {IMigration} from '../migrater';
 import {InitMigration} from './000_init';
 import {UserMigration} from './001_user';
+import {RegionIMigration} from './003_region';
 
 //定义好的数据迁移任务放入到数组中,顺序任意
 export const migrations: IMigration[] = [
   new InitMigration(),
-  new UserMigration()
+  new UserMigration(),
+  new RegionIMigration()
 ];
