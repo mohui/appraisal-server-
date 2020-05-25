96644ad7c95f0631debfc1d47e1176263664ff94
吴成浩 <wuchenghao@bjknrt.com>
2020-05-25 15:12:03 +0800

    修改规则和查询规则列表的接口

--- src/api/check-system.js
+++ src/api/check-system.js
@@ -1,18 +1,19 @@
 import {CheckRuleModel, CheckSystemModel} from '../database/model';
-import {should, validate} from 'kato-server';
+import {KatoCommonError, should, validate} from 'kato-server';
+import {appDB} from '../app';
 
 export default class CheckSystem {
   @validate(
     should.object({
-      name: should
+      checkName: should
         .string()
         .required()
         .description('考核系统名')
     })
   )
   async add(params) {
-    const {name} = params;
-    return await CheckSystemModel.create({checkName: name, total: 0});
+    const {checkName} = params;
+    return await CheckSystemModel.create({checkName, total: 0});
   }
 
   @validate(
@@ -21,15 +22,87 @@ export default class CheckSystem {
         .string()
         .required()
         .description('考核系统id'),
-      name: should
+      ruleName: should
         .string()
         .required()
         .description('规则名称')
     })
   )
   async addRule(params) {
-    const {checkId, name} = params;
-    return await CheckRuleModel.create({ruleName: name, checkId: checkId});
+    const {checkId, ruleName} = params;
+    return await CheckRuleModel.create({ruleName, checkId: checkId});
+  }
+
+  @validate(
+    should.object({
+      ruleId: should.string().required(),
+      ruleName: should
+        .string()
+        .required()
+        .description('规则名称'),
+      parentRuleId: should.string().description('父规则id'),
+      checkId: should
+        .string()
+        .required()
+        .description('考核体系id'),
+      ruleScore: should
+        .number()
+        .required()
+        .description('得分'),
+      checkStandard: should
+        .string()
+        .required()
+        .description('考核标准'),
+      checkMethod: should
+        .string()
+        .required()
+        .description('考核方法'),
+      status: should
+        .string()
+        .required()
+        .description('状态'),
+      standardIds: should
+        .array()
+        .items(should.string())
+        .allow([])
+        .description('关联指标id')
+    })
+  )
+  async updateRule(params) {
+    const {
+      ruleId,
+      ruleName,
+      parentRuleId = '',
+      checkId = '',
+      ruleScore = '',
+      checkStandard = '',
+      checkMethod = '',
+      status = '',
+      standardIds = []
+    } = params;
+    return appDB.transaction(async () => {
+      //查询规则,并锁定
+      let rule = await CheckRuleModel.findOne({where: {ruleId}, lock: true});
+      if (!rule) throw new KatoCommonError('该规则不存在');
+      //进行修改操作
+      return await CheckRuleModel.update(
+        {
+          ruleName,
+          parentRuleId,
+          checkId,
+          ruleScore,
+          checkMethod,
+          checkStandard,
+          status,
+          standardIds
+        },
+        {where: {ruleId}}
+      );
+    });
+  }
+
+  async listRule() {
+    return await CheckRuleModel.findAll({include: CheckSystemModel});
   }
 
   async list() {
