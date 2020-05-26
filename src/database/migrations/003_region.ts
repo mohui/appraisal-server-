55a9cfc59b64f972487a01e26434846cf5f1c428
吴成浩 <wuchenghao@bjknrt.com>
2020-05-26 14:10:17 +0800

    调整行政机构的建表脚本

--- src/database/migrations/003_region.ts
+++ src/database/migrations/003_region.ts
@@ -6,45 +6,29 @@ export class RegionIMigration implements IMigration {
   version = 3;
 
   async up(client: ExtendedSequelize): Promise<void> {
-    await client.execute(`-- 行政编码表
-DROP TABLE IF EXISTS "region";
-CREATE TABLE IF NOT EXISTS "region"
-(
-    "code"       varchar(255), -- 行政编码
-    "name"       varchar(255), -- 行政名称
-    "level"      int,          -- 行政级别 1: 省; 2: 市; 3: 县区; 4: 乡镇; 5: 村; 6. 组
-    "parent"     varchar(255), -- 上级行政编码
-    "created_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    primary key (code)
-);
+    await client.execute(`
+        CREATE TABLE IF NOT EXISTS "region" -- 行政编码表
+        (
+            "code"       varchar(255), -- 行政编码
+            "name"       varchar(255), -- 行政名称
+            "level"      int,          -- 行政级别 1: 省; 2: 市; 3: 县区; 4: 乡镇; 5: 村; 6. 组
+            "parent"     varchar(255), -- 上级行政编码
+            "created_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
+            "updated_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
+            primary key (code)
+        );
 
--- 机构表
-DROP TABLE IF EXISTS "hospital";
-CREATE TABLE IF NOT EXISTS "hospital"
-(
-    "id"         uuid,                                                                                 -- id
-    "name"       varchar(255),                                                                         -- 名称
-    "region"     varchar(255)                                       NOT NULL REFERENCES region (code), -- 机构所属行政编码
-    "parent"     uuid,                                                                                 -- 上级机构
-    "created_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    primary key (id)
-);
-
--- 机构映射表
-
-DROP TABLE IF EXISTS "hospital_mapping";
-CREATE TABLE IF NOT EXISTS "hospital_mapping"
-(
-    id           uuid,         -- id
-    h_id         uuid,         -- 机构表id
-    hisHospId    varchar(255), -- his 机构编码
-    hisid        varchar(255), -- his编码
-    "created_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
-    primary key (id)
-);`);
+        CREATE TABLE IF NOT EXISTS "hospital" -- 机构表
+        (
+            "id"         uuid,                                                       -- id
+            "name"       varchar(255),                                               -- 名称
+            "region"     varchar(255)  NOT NULL REFERENCES region (code),            -- 机构所属行政编码
+            "parent"     uuid,                                                       -- 上级机构
+            "created_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
+            "updated_at" TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP NOT NULL,
+            primary key (id)
+        );
+   `);
   }
 
   async down(client: ExtendedSequelize): Promise<void> {
