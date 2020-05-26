8df0636f83b646d2445d55134e3aba858a0189de
吴成浩 <wuchenghao@bjknrt.com>
2020-05-26 10:24:31 +0800

    完成行政机构列表查询的条件筛选

--- src/api/region.js
+++ src/api/region.js
@@ -1,33 +1,77 @@
 import {RegionModel, HospitalModel} from '../database/model';
 import {should, validate} from 'kato-server';
+import {Op} from 'sequelize';
 
 export default class Region {
   @validate(
     should
       .object({
         pageSize: should.number(),
-        pageNo: should.number()
+        pageNo: should.number(),
+        level: should.number(),
+        parent: should.string(),
+        name: should.string()
       })
       .allow(null)
   )
   async list(params) {
-    const {pageSize = 20, pageNo = 1} = params || {};
+    const {pageSize = 20, pageNo = 1, level, parent, name} = params || {};
+    let whereOptions = {};
+    if (level) whereOptions['level'] = level;
+    if (parent) whereOptions['parent'] = parent;
+    if (name) whereOptions['name'] = {[Op.like]: `%${name}%`};
     return await RegionModel.findAndCountAll({
       attributes: {
         exclude: ['deleted_at']
       },
+      where: whereOptions,
       paranoid: false,
+      distinct: true,
       offset: pageNo - 1,
-      limit: pageSize
+      limit: pageSize,
+      include: {
+        model: HospitalModel,
+        paranoid: false,
+        attributes: {
+          exclude: ['deleted_at']
+        }
+      }
     });
   }
 
-  async listHospital() {
+  @validate(
+    should
+      .object({
+        pageSize: should.number(),
+        pageNo: should.number(),
+        region: should.string(),
+        parent: should.string(),
+        name: should.string()
+      })
+      .allow(null)
+  )
+  async listHospital(params) {
+    const {pageSize = 20, pageNo = 1, region, parent, name} = params || {};
+    let whereOptions = {};
+    if (region) whereOptions['region'] = region;
+    if (parent) whereOptions['parent'] = parent;
+    if (name) whereOptions['name'] = {[Op.like]: `%${name}%`};
     return await HospitalModel.findAndCountAll({
       attributes: {
         exclude: ['deleted_at']
       },
-      paranoid: false
+      where: whereOptions,
+      distinct: true,
+      paranoid: false,
+      offset: pageNo - 1,
+      limit: pageSize,
+      include: {
+        model: RegionModel,
+        paranoid: false,
+        attributes: {
+          exclude: ['deleted_at']
+        }
+      }
     });
   }
 }
