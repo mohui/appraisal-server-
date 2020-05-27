c82229ee833bb99c20aca22beb0acab66b1f110f
吴成浩 <wuchenghao@bjknrt.com>
2020-05-27 15:14:59 +0800

    完成hospital.list接口

---
+++ src/api/hospital.js
@@ -0,0 +1,27 @@
+import {HospitalModel, RegionModel} from '../database/model';
+import {should, validate} from 'kato-server';
+
+export default class Hospital {
+  @validate(
+    should
+      .string()
+      .required()
+      .description('父级机构的id')
+  )
+  async list(parent) {
+    HospitalModel.findAll({
+      attributes: {
+        exclude: ['deleted_at']
+      },
+      where: {parent},
+      paranoid: false,
+      include: {
+        model: RegionModel,
+        paranoid: false,
+        attributes: {
+          exclude: ['deleted_at']
+        }
+      }
+    });
+  }
+}
--- src/api/region.js
+++ src/api/region.js
@@ -13,30 +13,12 @@ export default class Region {
   }
 
   @validate(should.string().description('通过code查询区域下的机构'))
-  async listHospitalByCode(code) {
+  async listHospital(code) {
     return await HospitalModel.findAll({
       attributes: {
         exclude: ['deleted_at']
       },
       where: {region: code},
-      paranoid: false,
-      include: {
-        model: RegionModel,
-        paranoid: false,
-        attributes: {
-          exclude: ['deleted_at']
-        }
-      }
-    });
-  }
-
-  @validate(should.string().description('通过上级机构查询其下的机构'))
-  async listHospital(parent) {
-    return await HospitalModel.findAll({
-      attributes: {
-        exclude: ['deleted_at']
-      },
-      where: {parent},
       paranoid: false,
       include: {
         model: RegionModel,
