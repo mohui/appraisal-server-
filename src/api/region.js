b1397023b6f2a2eea2bea63058afb2bf20b82125
吴成浩 <wuchenghao@bjknrt.com>
2020-05-27 11:32:01 +0800

    重写行政和机构的查询接口

--- src/api/region.js
+++ src/api/region.js
@@ -1,74 +1,36 @@
 import {RegionModel, HospitalModel} from '../database/model';
 import {should, validate} from 'kato-server';
-import {Op} from 'sequelize';
 
 export default class Region {
-  @validate(
-    should
-      .object({
-        pageSize: should.number(),
-        pageNo: should.number(),
-        level: should.number(),
-        parent: should.string(),
-        name: should.string(),
-        code: should.string()
-      })
-      .allow(null)
-  )
-  async list(params) {
-    const {pageSize = 20, pageNo = 1, level, parent, name, code} = params || {};
-    let whereOptions = {};
-    if (level) whereOptions['level'] = level;
-    if (parent) whereOptions['parent'] = parent;
-    if (name) whereOptions['name'] = {[Op.like]: `%${name}%`};
-    if (code) whereOptions['code'] = code;
-    return await RegionModel.findAndCountAll({
-      attributes: {
-        exclude: ['deleted_at']
-      },
-      where: whereOptions,
+  //通过code查询下一级行政区域
+  @validate(should.string().required())
+  async list(code) {
+    return await RegionModel.findAll({
       paranoid: false,
-      distinct: true,
-      offset: pageNo - 1,
-      limit: pageSize,
-      include: {
-        model: HospitalModel,
-        paranoid: false,
-        attributes: {
-          exclude: ['deleted_at']
-        }
-      }
+      attributes: {exclude: ['deleted_at']},
+      where: {parent: code}
     });
   }
 
   @validate(
     should
       .object({
-        pageSize: should.number(),
-        pageNo: should.number(),
-        region: should.string(),
-        parent: should.string(),
-        name: should.string(),
-        id: should.string()
+        code: should.string().description('通过code查询区域下的机构'),
+        parent: should.string().description('通过上级机构查询其名下的机构')
       })
       .allow(null)
   )
   async listHospital(params) {
-    const {pageSize = 20, pageNo = 1, region, parent, name, id} = params || {};
+    const {code, parent} = params || {};
     let whereOptions = {};
-    if (region) whereOptions['region'] = region;
+    if (code) whereOptions['region'] = code;
     if (parent) whereOptions['parent'] = parent;
-    if (name) whereOptions['name'] = {[Op.like]: `%${name}%`};
-    if (id) whereOptions['id'] = id;
-    return await HospitalModel.findAndCountAll({
+    return await HospitalModel.findAll({
       attributes: {
         exclude: ['deleted_at']
       },
       where: whereOptions,
-      distinct: true,
       paranoid: false,
-      offset: pageNo - 1,
-      limit: pageSize,
       include: {
         model: RegionModel,
         paranoid: false,
