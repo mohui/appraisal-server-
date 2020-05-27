9a324bf16eca93bedb90892e237afabeef4a0b5e
戴凯杰 <daikaijie@bjknrt.com>
2020-05-27 22:50:18 +0800

    调整rule_tag表结构, 添加"分值"字段

--- src/database/model/rule-tag.ts
+++ src/database/model/rule-tag.ts
@@ -4,18 +4,27 @@ import {
   Column,
   Comment,
   DataType,
+  Default,
   ForeignKey,
   Model,
+  PrimaryKey,
   Table
 } from 'sequelize-typescript';
 import {CheckRule} from './check_rule';
+import {UUIDV4} from 'sequelize';
 
 @Table({tableName: 'rule_tag'})
 export class RuleTag extends Model<RuleTag> {
+  @PrimaryKey
+  @Default(UUIDV4)
+  @Comment('主键id')
+  @Column(DataType.UUID)
+  id: string;
+
   @Comment('考核细则id')
   @AllowNull(false)
   @ForeignKey(() => CheckRule)
-  @Column({field: 'rule_id', type: DataType.UUID})
+  @Column({field: 'rule', type: DataType.UUID})
   ruleId: string;
 
   @BelongsTo(() => CheckRule)
@@ -34,4 +43,8 @@ export class RuleTag extends Model<RuleTag> {
   @Comment('参考值; 个别计算方式, 需要参考值')
   @Column
   baseline: number;
+
+  @Comment('分值')
+  @Column(DataType.FLOAT)
+  score: number;
 }
