d2652b57ad624e5b79ff3e17032281904027866b
吴成浩 <wuchenghao@bjknrt.com>
2020-05-29 17:42:39 +0800

    基础数据list接口

--- src/api/basic-tag.js
+++ src/api/basic-tag.js
@@ -1,7 +1,8 @@
-import {BasicTagDataModel} from '../database/model';
+import {BasicTagDataModel, HospitalModel} from '../database/model';
 import {appDB} from '../app';
 import {should, validate} from 'kato-server';
 import dayjs from 'dayjs';
+import {BasicTags} from '../../common/rule-score';
 
 export default class BasicTag {
   //设置基础数据
@@ -26,5 +27,54 @@ export default class BasicTag {
       tag.value = value;
       return await tag.save();
     });
+  }
+
+  @validate(
+    should
+      .string()
+      .required()
+      .description('大类指标的code')
+  )
+  async list(tagCode) {
+    //获取大类指标下的所有的小类
+    const childrenTag = BasicTags.find(bt => bt.code === tagCode).children;
+
+    //取出所有的机构
+    const centerHospitals = await HospitalModel.findAll({
+      where: {region: '340203'}
+    });
+
+    return await Promise.all(
+      centerHospitals.map(async h => {
+        let childrenHospital = await HospitalModel.findAll({
+          where: {parent: h.id}
+        });
+        //以机构和小类code进行查询
+        childrenHospital = await Promise.all(
+          childrenHospital.map(async child => {
+            child = child.toJSON();
+            let tags = await Promise.all(
+              childrenTag.map(async tag => {
+                //查询某个机构在某个指标的数据
+                const basicData = await BasicTagDataModel.findOne({
+                  where: {code: tag.code, hospitalId: child.id}
+                });
+                return basicData
+                  ? basicData
+                  : {
+                      hospitalId: child.id,
+                      code: tag.code,
+                      value: 0,
+                      year: dayjs().year()
+                    };
+              })
+            );
+            tags.forEach(tag => (child[tag.code] = tag));
+            return child;
+          })
+        );
+        return {...h.toJSON(), children: childrenHospital};
+      })
+    );
   }
 }
