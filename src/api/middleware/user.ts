f853d8525127b21dff26090e4eae307ee656cb9e
吴成浩 <wuchenghao@bjknrt.com>
2020-06-01 15:35:20 +0800

    修改User中间件

--- src/api/middleware/user.ts
+++ src/api/middleware/user.ts
@@ -1,38 +1,14 @@
-import {Context} from 'kato-server';
-import {knrtDB} from '../../app';
+import {Context} from '../context';
+import {UserModel} from '../../database/model';
 
 export async function UserMiddleware(ctx: Context | any, next: Function) {
   try {
     const token = ctx.req.header('token');
-
     if (token) {
-      const users = await knrtDB.execute(
-        `
-        select user.id,
-               user.username,
-               user.token,
-               user.status,
-               user.usertype,
-               user.realname,
-               user.duty,
-               user.institutions,
-               user.headpic,
-               access.level,
-               access.sys_code
-        from leader_user user
-               left join leader_access access on user.id = access.u_id
-        where user.token = ?
-          and user.status = ?
-      `,
-        token,
-        1
-      );
-
-      ctx.user = users[0];
+      ctx.user = await UserModel.findOne({where: {id: token}});
     }
   } catch (e) {
     console.log(e);
   }
-
   await next();
 }
