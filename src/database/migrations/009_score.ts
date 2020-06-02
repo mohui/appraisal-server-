322b5872c6172bd9011ba83400e355e85b19cee3
戴凯杰 <daikaijie@bjknrt.com>
2020-06-02 17:41:21 +0800

    修正迁移脚本

--- src/database/migrations/009_score.ts
+++ src/database/migrations/009_score.ts
@@ -8,7 +8,7 @@ export class ScoreMigration implements IMigration {
   async up(client: ExtendedSequelize): Promise<void> {
     // language=PostgreSQL
     await client.execute(`
-      CREATE TABLE IF NOT EXISTS "mark-hospital"
+      CREATE TABLE IF NOT EXISTS mark_hospital
       (
         "hospital"   UUID REFERENCES "hospital" ("id") ON DELETE NO ACTION ON UPDATE CASCADE,
         "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -18,10 +18,10 @@ export class ScoreMigration implements IMigration {
         "S03"        INTEGER                  DEFAULT 0,
         PRIMARY KEY ("hospital")
       );
-      COMMENT ON COLUMN "mark-hospital"."hospital" IS '机构';
-      COMMENT ON COLUMN "mark-hospital"."S00" IS '健康档案总数';
-      COMMENT ON COLUMN "mark-hospital"."S23" IS '健康档案规范数';
-      COMMENT ON COLUMN "mark-hospital"."S03" IS '健康档案使用数';
+      COMMENT ON COLUMN mark_hospital."hospital" IS '机构';
+      COMMENT ON COLUMN mark_hospital."S00" IS '健康档案总数';
+      COMMENT ON COLUMN mark_hospital."S23" IS '健康档案规范数';
+      COMMENT ON COLUMN mark_hospital."S03" IS '健康档案使用数';
     `);
   }
 
