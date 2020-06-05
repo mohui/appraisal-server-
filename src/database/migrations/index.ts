faf06d3dbc1d9b00c1f79acfdfb91fdd547aaba6
吴成浩 <wuchenghao@bjknrt.com>
2020-06-05 16:52:10 +0800

    添加考核与机构关系表的脚本并升级迁移脚本

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -9,6 +9,7 @@ import {RuleScoreMigration} from './006_rule_score';
 import {UserRegionMigration} from './007_user_region';
 import {TagEditorMigration} from './008_tag_editor';
 import {ScoreMigration} from './009_score';
+import {CheckHospitalMigration} from './010_check_hospital';
 
 //定义好的数据迁移任务放入到数组中,顺序任意
 export const migrations: IMigration[] = [
@@ -21,5 +22,6 @@ export const migrations: IMigration[] = [
   new RuleScoreMigration(),
   new UserRegionMigration(),
   new TagEditorMigration(),
-  new ScoreMigration()
+  new ScoreMigration(),
+  new CheckHospitalMigration()
 ];
