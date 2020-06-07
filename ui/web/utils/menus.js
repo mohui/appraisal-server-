c19cb1336bc49007abd87272cc81af8ad6512ece
戴凯杰 <daikaijie@bjknrt.com>
2020-06-07 16:42:57 +0800

    删除测试路由菜单

--- ui/web/utils/menus.js
+++ ui/web/utils/menus.js
@@ -9,19 +9,6 @@ module.exports = [
     icon: 'el-icon-s-home'
   },
   {
-    index: 'test',
-    label: '测试页',
-    icon: 'el-icon-edit',
-    children: [
-      {
-        index: 'test',
-        router: '/test',
-        label: '子测试页',
-        icon: 'el-icon-user'
-      }
-    ]
-  },
-  {
     index: 'userManage',
     permission: [Permission.USER_INDEX, Permission.ROLE_INDEX],
     label: '用户管理',
