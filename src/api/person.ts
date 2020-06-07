accd8f69d8271ca1fb9d3afaabf875b029e91ded
戴凯杰 <daikaijie@bjknrt.com>
2020-06-07 20:35:41 +0800

    默认查询弋江区数据, 并修复分页bug

--- src/api/person.ts
+++ src/api/person.ts
@@ -5,12 +5,17 @@ export default class Person {
   async list(pageSize, pageNo) {
     const limit = pageSize;
     const offset = (pageNo - 1 || 0) * limit;
+    const his = '340203';
     // language=PostgreSQL
     const count = (
       await etlDB.query(
         `select count(1) as count
-           from mark_person`,
+           from mark_person mp
+                  left join view_personinfo vp on mp.personnum = vp.personnum and mp.hisid = vp.hisid
+           where mp.hisid = ?
+             and vp.vouchertype = '1'`,
         {
+          replacements: [his],
           type: QueryTypes.SELECT
         }
       )
@@ -19,17 +24,22 @@ export default class Person {
     // language=PostgreSQL
     const person = await etlDB.query(
       `
-        select vp.name, vp.idcardno as "idCard", mp."S03", mp."S23", vh0.hospname as hospital0, vh1.hospname as hospital1
+        select vp.name,
+               vp.idcardno  as "idCard",
+               mp."S03",
+               mp."S23",
+               vh0.hospname as hospital0,
+               vh1.hospname as hospital1
         from mark_person mp
                left join view_personinfo vp on mp.personnum = vp.personnum and mp.hisid = vp.hisid
                left join view_hospital vh0 on mp.hisid = vh0.hisid and vp.operatorid = vh0.hospid
                left join view_hospital vh1 on mp.hisid = vh1.hisid and vp.adminorganization = vh1.hospid
-        where mp.hisid = '340203'
+        where mp.hisid = ?
           and vp.vouchertype = '1'
         limit ? offset ?
       `,
       {
-        replacements: [limit, offset],
+        replacements: [his, limit, offset],
         type: QueryTypes.SELECT
       }
     );
