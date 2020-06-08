e9ef8c618fd590a6e46d8153dab8f10e44131a8f
吴成浩 <wuchenghao@bjknrt.com>
2020-06-08 17:53:04 +0800

    用户中间件完善用户的信息获取

--- src/api/middleware/user.ts
+++ src/api/middleware/user.ts
@@ -1,11 +1,16 @@
 import {Context} from '../context';
-import {HospitalModel, UserModel} from '../../database/model';
+import {
+  HospitalModel,
+  RegionModel,
+  RoleModel,
+  UserModel
+} from '../../database/model';
 
 export async function UserMiddleware(ctx: Context | any, next: Function) {
   try {
     const token = ctx.req.header('token');
     if (token) {
-      ctx.user = (
+      const user = (
         await UserModel.findOne({
           where: {id: Context.req.headers.token},
           include: [
@@ -14,10 +19,31 @@ export async function UserMiddleware(ctx: Context | any, next: Function) {
               through: {
                 attributes: []
               }
+            },
+            {
+              model: RoleModel,
+              through: {
+                attributes: []
+              }
+            },
+            {
+              model: RegionModel
             }
           ]
         })
       ).toJSON();
+      //该用户的默认code
+      user.code =
+        user.hospitals.length === 1 ? user.hospitals[0].id : user.regionId;
+      //用户权限去重整理
+      user.permissions = [
+        ...new Set(
+          user.roles
+            .map(it => it.permissions)
+            .reduce((result, next) => result.concat(next), [])
+        )
+      ];
+      ctx.user = user;
     }
   } catch (e) {
     console.log(e);
