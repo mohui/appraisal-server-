b768445c20af9114060c1e5125211b5af70e30fc
谢高军 <xiegaojun@bjknrt.com>
2020-06-09 13:55:52 +0800

    setting中给user添加isRegion字段，区分该用户是否为地区权限

--- ui/web/plugins/setting.js
+++ ui/web/plugins/setting.js
@@ -14,6 +14,8 @@ export const settingPlugin = {
           const user = await this.$api.User.profile(getToken());
           user.code =
             user.hospitals.length === 1 ? user.hospitals[0].id : user.regionId;
+          //用户是否为地区权限
+          user.isRegion = user.code === user.regionId ? true : false;
           this.user = user;
           this.permissions = [
             ...new Set(this.user.roles.map(it => it.permissions).flat())
