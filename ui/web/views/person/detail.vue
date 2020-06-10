d173374acf041a114d1becb86f2eabf8066549e2
戴凯杰 <daikaijie@bjknrt.com>
2020-06-11 00:06:28 +0800

    添加个人档案详情页

---
+++ ui/web/views/person/detail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="height: 100%;">
+    <el-card
+      class="box-card"
+      style="height: 100%;"
+      shadow="never"
+      :body-style="{
+        height: 'calc(100% - 110px)',
+        display: 'flex',
+        'flex-direction': 'column'
+      }"
+    >
+      <div slot="header" class="clearfix">
+        <span>个人档案详情</span>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detail',
+  data() {
+    return {
+      id: null
+    };
+  },
+  created() {
+    const id = this.$route.query.id;
+    console.log(id);
+    if (!id) this.$router.push('/person');
+    this.id = id;
+  },
+  asyncComputed: {
+    serverData: {
+      async get() {
+        return await this.$api.Person.detail(this.id);
+      },
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style scoped></style>
