ee2f9329c21240bfda9ebfdba69f40b7b66ffab3
谢高军 <xiegaojun@bjknrt.com>
2020-06-14 16:19:49 +0800

    将routes的redirect设置为appraisal-result-institutions

--- ui/web/router.js
+++ ui/web/router.js
@@ -10,7 +10,7 @@ const router = new Router({
     {
       path: '/',
       component: Layout,
-      redirect: '/home',
+      redirect: '/appraisal-result-institutions',
       children: [
         {
           path: 'home',
