42692d08c9e5b3e80cc6f3ed5bba3fe2c9e181b1
谢高军 <xiegaojun@bjknrt.com>
2020-06-15 10:44:29 +0800

    根据用户权限判断route到地区页还是机构页

--- ui/web/views/home/index.vue
+++ ui/web/views/home/index.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>首页</div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  created() {
+    //根据用户权限判断route到地区页还是机构页
+    if (this.$settings.user.isRegion) {
+      this.$router.replace({path: 'appraisal-result-area'});
+    } else {
+      this.$router.replace({path: 'appraisal-result-institutions'});
+    }
+  }
 };
 </script>
 
