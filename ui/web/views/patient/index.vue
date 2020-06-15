61393b6aadc4febce668763f75fb5b03eba7749b
宋彦强 <songyanqiang@bjknrt.com>
2020-06-15 14:28:13 +0800

    如患者没有相关疾病则不显示相应记录列表

--- ui/web/views/patient/index.vue
+++ ui/web/views/patient/index.vue
@@ -4,59 +4,86 @@
       <el-col :span="6" :xs="24">
         <card :patient="document" />
       </el-col>
-
       <el-col :span="18" :xs="24" style="height: 100%;">
         <el-card
           shadow="never"
           style="height: 100%;"
           :body-style="{
-            height: 'calc(100% - 100px)',
-            display: 'flex',
-            'flex-direction': 'column'
+            height: 'calc(100% - 40px)'
           }"
         >
-          <el-tabs v-model="activeTab">
-            <el-tab-pane label="体检记录" name="physical">
-              <div class="notes" v-for="(item, index) of healthy" :key="index">
-                <div class="notes-block">
-                  <span class="hospital">体检时间：{{ item.updateAt }}</span>
+          <el-tabs v-model="activeTab" class="patient-tab-list">
+            <el-tab-pane
+              label="体检记录"
+              name="physical"
+              v-if="healthyList.length"
+            >
+              <div>
+                <div
+                  class="notes"
+                  v-for="(item, index) of healthyList"
+                  :key="index"
+                >
+                  <div class="notes-block">
+                    <span class="hospital">体检时间：{{ item.updateAt }}</span>
+                  </div>
+                  <p>
+                    身高：{{ item.stature }} 体重：{{ item.weight }} 体温：{{
+                      item.temperature
+                    }}
+                    症状：{{ item.symptom }}
+                  </p>
                 </div>
-                <p>
-                  身高：{{ item.stature }} 体重：{{ item.weight }} 体温：{{
-                    item.temperature
-                  }}
-                  症状：{{ item.symptom }}
-                </p>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="高血压随访记录" name="hypertension">
-              <div
-                class="notes"
-                v-for="(item, index) of hypertensions"
-                :key="index"
-              >
-                <div class="notes-block">
-                  <span class="hospital">随访医生：{{ item.doctor }}</span>
-                  <span class="visitTime">随访时间：{{ item.followDate }}</span>
+            <el-tab-pane
+              label="高血压随访记录"
+              name="hypertension"
+              v-if="hypertensions.length"
+            >
+              <div>
+                <div
+                  class="notes"
+                  v-for="(item, index) of hypertensions"
+                  :key="index"
+                >
+                  <div class="notes-block">
+                    <span class="hospital">随访医生：{{ item.doctor }}</span>
+                    <span class="visitTime"
+                      >随访时间：{{ item.followDate }}</span
+                    >
+                  </div>
+                  <p>
+                    随访方式：{{ item.followWay }}；收缩压：{{
+                      item.systolicPressure
+                    }}；舒张压：{{ item.assertPressure }}
+                  </p>
                 </div>
-                <p>
-                  随访方式：{{ item.followWay }}；收缩压：{{
-                    item.systolicPressure
-                  }}；舒张压：{{ item.assertPressure }}
-                </p>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="糖尿病随访记录" name="diabetes">
-              <div class="notes" v-for="(item, index) of diabetes" :key="index">
-                <div class="notes-block">
-                  <span class="hospital">随访医生：{{ item.doctor }}</span>
-                  <span class="visitTime">随访时间：{{ item.followDate }}</span>
+            <el-tab-pane
+              label="糖尿病随访记录"
+              name="diabetes"
+              v-if="diabetesList.length"
+            >
+              <div>
+                <div
+                  class="notes"
+                  v-for="(item, index) of diabetesList"
+                  :key="index"
+                >
+                  <div class="notes-block">
+                    <span class="hospital">随访医生：{{ item.doctor }}</span>
+                    <span class="visitTime"
+                      >随访时间：{{ item.followDate }}</span
+                    >
+                  </div>
+                  <p>
+                    随访方式：{{ item.followWay }}；空腹血糖：{{
+                      item.fastingGlucose
+                    }}；随机血糖：{{ item.postprandialGlucose }}
+                  </p>
                 </div>
-                <p>
-                  随访方式：{{ item.followWay }}；空腹血糖：{{
-                    item.fastingGlucose
-                  }}；随机血糖：{{ item.postprandialGlucose }}
-                </p>
               </div>
             </el-tab-pane>
           </el-tabs>
@@ -83,9 +110,32 @@ export default {
     if (!id) this.$router.push('/person');
     this.id = id;
   },
+  mounted() {
+    if (this.healthyList.length > 0) {
+      this.activeTab = 'physical';
+    } else if (this.hypertensions.length > 0) {
+      this.activeTab = 'hypertension';
+    } else if (this.diabetesList.length > 0) {
+      this.activeTab = 'diabetes';
+    }
+  },
   computed: {
     hypertensions() {
       return this.hypertension.map(it => {
+        it.followDate = it.followDate
+          ? it.followDate.$format('YYYY-MM-DD')
+          : '';
+        return it;
+      });
+    },
+    healthyList() {
+      return this.healthy.map(it => {
+        it.updateAt = it.updateAt ? it.updateAt.$format() : '';
+        return it;
+      });
+    },
+    diabetesList() {
+      return this.diabetes.map(it => {
         it.followDate = it.followDate
           ? it.followDate.$format('YYYY-MM-DD')
           : '';
@@ -154,7 +204,32 @@ export default {
 };
 </script>
 
-<style type="scss" scoped>
+<style lang="scss">
+.patient-tab-list {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .el-tabs__content {
+    height: 100%;
+    flex: 1;
+    .el-tab-pane {
+      height: 100%;
+      position: relative;
+
+      & > div {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        overflow-x: hidden;
+        overflow-y: auto;
+      }
+    }
+  }
+}
+</style>
+<style lang="scss" scoped>
 .notes {
   border-bottom: 1px solid #eee;
   margin-bottom: 10px;
