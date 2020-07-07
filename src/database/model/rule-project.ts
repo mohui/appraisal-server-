066d4c00daa77b1777796a573842b0b2c8f36689
吴成浩 <wuchenghao@bjknrt.com>
2020-07-07 13:34:00 +0800

    考核和工分项的model定义

---
+++ src/database/model/rule-project.ts
@@ -0,0 +1,35 @@
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  Comment,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table
+} from 'sequelize-typescript';
+import {CheckRule} from './check_rule';
+
+@Table({tableName: 'rule_project'})
+export class RuleProject extends Model<RuleProject> {
+  @Comment('考核小则id')
+  @AllowNull(false)
+  @PrimaryKey
+  @ForeignKey(() => CheckRule)
+  @Column({field: 'rule', type: DataType.UUID})
+  ruleId: string;
+
+  @BelongsTo(() => CheckRule)
+  rule: CheckRule;
+
+  @Comment('工分项')
+  @AllowNull(false)
+  @PrimaryKey
+  @Column({field: 'projectType', type: DataType.STRING})
+  projectId;
+
+  @Comment('工分名')
+  @Column({field: 'projectName', type: DataType.STRING})
+  projectName;
+}
