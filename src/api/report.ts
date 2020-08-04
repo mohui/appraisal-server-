8bf60af9aafda7174717001271ab1c7b88d7becd
戴凯杰 <daikaijie@bjknrt.com>
2020-08-04 15:09:16 +0800

    优化文件名显示

--- src/api/report.ts
+++ src/api/report.ts
@@ -2,6 +2,24 @@ import {ossClient} from '../../util/oss';
 import * as config from 'config';
 import * as path from 'upath';
 
+/**
+ * 语义化时间
+ *
+ * @param time 时间字符串, '2020Q1', '2020H1', '2020Q3', '2020'
+ */
+function displayTime(time) {
+  if (time.includes('Q')) {
+    const times = time.split('Q');
+    return `${times[0]}年第${times[1]}季度报告`;
+  }
+  if (time.includes('H')) {
+    const times = time.split('H');
+    return `${times[0]}年${times[1] === 1 ? '上' : '下'}半年报告`;
+  }
+
+  return `${time}年报告`;
+}
+
 export default class Report {
   /**
    * 获取报告列表
@@ -22,10 +40,10 @@ export default class Report {
         })
       )?.objects ?? []
     )
-      .filter(it => it.name.includes(`${id}_`)) // 文件名约定为 id_时间.docx
+      .filter(it => it.name.includes(`${id}_`)) // 文件名约定为 id_time.docx
       .map(it => ({
         id: it.name,
-        name: path.parse(it.name).name,
+        name: displayTime(path.parse(it.name).name.split('_')[1]),
         url: it.url
       }));
   }
