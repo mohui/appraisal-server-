82c3f2b2ffbc474532d24505e81399d8546231c0
戴凯杰 <daikaijie@bjknrt.com>
2020-08-04 14:35:05 +0800

    添加文件名返回值

--- src/api/report.ts
+++ src/api/report.ts
@@ -1,5 +1,6 @@
 import {ossClient} from '../../util/oss';
 import * as config from 'config';
+import * as path from 'upath';
 
 export default class Report {
   /**
@@ -8,6 +9,7 @@ export default class Report {
    * @param id 地区或机构id
    * @return {
    *   id: 文件id
+   *   name: 文件名
    *   url: 文件下载地址
    * }
    */
@@ -23,6 +25,7 @@ export default class Report {
       .filter(it => it.name.includes(`${id}_`)) // 文件名约定为 id_时间.docx
       .map(it => ({
         id: it.name,
+        name: path.parse(it.name).name,
         url: it.url
       }));
   }
