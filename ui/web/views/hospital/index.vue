cd5c0149ca24461e5ae068472d07fd18312aabb7
谢高军 <xiegaojun@bjknrt.com>
2020-09-01 16:39:14 +0800

    table表格改成树形显示

--- ui/web/views/hospital/index.vue
+++ ui/web/views/hospital/index.vue
@@ -48,54 +48,17 @@
       </el-form>
       <el-table
         size="mini"
-        ref="expandTable"
         :data="hospitalListData"
         height="100%"
         style="flex-grow: 1;"
-        :row-style="{cursor: 'pointer'}"
-        @row-click="handleRowClickExpand"
-        :header-cell-style="{
-          background: '#F3F4F7',
-          color: '#555',
-          textAlign: 'center'
-        }"
+        row-key="uuid"
+        lazy
+        :load="load"
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
       >
-        <el-table-column type="expand">
-          <template slot-scope="props">
-            <el-table
-              border
-              size="mini"
-              :data="props.row.children"
-              @row-click="handleCellClick"
-              :cell-class-name="cellClassHover"
-            >
-              <el-table-column align="center" label="序号">
-                <template slot-scope="scope">
-                  {{ scope.$index + 1 }}
-                </template>
-              </el-table-column>
-              <el-table-column align="center" label="机构名称" prop="name">
-              </el-table-column>
-              <el-table-column
-                align="center"
-                label="校正后总工分值"
-                prop="correctWorkPointFormat"
-              >
-              </el-table-column>
-              <el-table-column
-                align="center"
-                label="质量系数"
-                prop="rateFormat"
-              >
-              </el-table-column>
-              <el-table-column align="center" label="金额" prop="budgetFormat">
-              </el-table-column>
-            </el-table>
-          </template>
-        </el-table-column>
         <el-table-column align="center" label="序号">
           <template slot-scope="scope">
-            {{ scope.$index + 1 }}
+            {{ scope.row.uuid }}
           </template>
         </el-table-column>
         <el-table-column align="center" label="机构名称" prop="name">
@@ -150,54 +113,13 @@ export default {
       return (
         this.hospitalListServerData
           //添加格式化数据
-          .map(item => {
+          .map((item, index) => {
             item.correctWorkPointFormat = Math.round(item.correctWorkPoint);
             item.rateFormat = (item.rate * 100).toFixed(2) + '%';
             item.budgetFormat = item.budget.toFixed(2);
+            item.uuid = index + 1;
+            item.hasChildren = true;
             return item;
-          })
-          //过滤，只取一级机构（name以"服务中心"和"卫生院"结尾）的值
-          .filter(
-            item =>
-              item.name.endsWith('服务中心') || item.name.endsWith('卫生院')
-          )
-          //添加child
-          .map(item => {
-            const returnValue = Object.assign({}, item, {
-              children: [
-                item,
-                ...this.hospitalListServerData.filter(
-                  it => it.parent === item.id
-                )
-              ]
-            });
-            //累加校正后的总工分值
-            returnValue.correctWorkPoint = returnValue.children.reduce(
-              (result, current) => (result += current.correctWorkPoint),
-              0
-            );
-            //格式化累加校正后的总工分值
-            returnValue.correctWorkPointFormat = Math.round(
-              returnValue.correctWorkPoint
-            );
-            //累加金额数
-            returnValue.budget = returnValue.children.reduce(
-              (result, current) => (result += current.budget),
-              0
-            );
-            //格式化累加金额数
-            returnValue.budgetFormat = returnValue.budget.toFixed(2);
-            //累加质量系数
-            returnValue.rate = returnValue.children.reduce(
-              (result, current) => (result += current.rate),
-              0
-            );
-            //取累加后质量系数的平均值
-            returnValue.rate = returnValue.rate / returnValue.children.length;
-            //格式化质量系数平均值
-            returnValue.rateFormat = (returnValue.rate * 100).toFixed(2) + '%';
-
-            return returnValue;
           })
       );
     }
@@ -215,8 +137,19 @@ export default {
     }
   },
   methods: {
-    handleRowClickExpand(row) {
-      this.$refs.expandTable.toggleRowExpansion(row);
+    async load(tree, treeNode, resolve) {
+      console.log(tree, treeNode, resolve);
+      let result = (await this.$api.Region.listAllHospital(tree.code)).map(
+        (item, index) => {
+          item.correctWorkPointFormat = Math.round(item.correctWorkPoint);
+          item.rateFormat = (item.rate * 100).toFixed(2) + '%';
+          item.budgetFormat = item.budget.toFixed(2);
+          item.uuid = tree.uuid + '.' + (index + 1);
+          item.hasChildren = true;
+          return item;
+        }
+      );
+      resolve(result);
     },
     //设置标题可点击样式
     cellClassHover({columnIndex}) {
