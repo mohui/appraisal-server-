44b299d23698bb6617ca9b908f6ba59749ea8b3c
吴成浩 <wuchenghao@bjknrt.com>
2020-10-26 10:55:05 +0800

    补充打分相关的表里面的check_id字段的值的迁移脚本

--- src/database/migrations/024_check_system.ts
+++ src/database/migrations/024_check_system.ts
@@ -18,6 +18,16 @@ export class AlterCheckSystemMigration implements IMigration {
       alter table "report_hospital"
         add column if not exists "check_id" uuid default null
           references "check_system" ("check_id") on delete set null on update cascade;
+
+      --补上跑分历史记录的check_id的值
+      update report_hospital_history rhh set "check_id" = ch.check_system
+      from (select check_system,hospital from check_hospital) as ch
+      where rhh.hospital = ch.hospital;
+
+      --补上跑分结果的check_id的值
+      update report_hospital rh set "check_id" = ch.check_system
+      from (select check_system,hospital from check_hospital) as ch
+      where rh.hospital = ch.hospital;
     `);
   }
 
