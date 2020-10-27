85e9deaf42398bcb7c9f9938ef3c8cc483b9abe3
吴成浩 <wuchenghao@bjknrt.com>
2020-10-27 17:18:25 +0800

    格式化代码

--- src/database/migrations/024_check_system.ts
+++ src/database/migrations/024_check_system.ts
@@ -20,32 +20,44 @@ export class AlterCheckSystemMigration implements IMigration {
           references "check_system" ("check_id") on update cascade on delete cascade;
 
       --补上跑分历史记录的check_id的值
-      update report_hospital_history rhh set "check_id" = ch.check_system
-      from (select check_system,hospital from check_hospital ch
-            left join check_system cs on ch.check_system = cs.check_id
-            where cs.check_type=1) as ch
+      update report_hospital_history rhh
+      set "check_id" = ch.check_system
+      from (select check_system, hospital
+            from check_hospital ch
+                   left join check_system cs on ch.check_system = cs.check_id
+            where cs.check_type = 1) as ch
       where rhh.hospital = ch.hospital;
 
       --补上跑分结果的check_id的值
-      update report_hospital rh set "check_id" = ch.check_system
-      from (select check_system,hospital from check_hospital ch
-            left join check_system cs on ch.check_system = cs.check_id
-            where cs.check_type=1) as ch
+      update report_hospital rh
+      set "check_id" = ch.check_system
+      from (select check_system, hospital
+            from check_hospital ch
+                   left join check_system cs on ch.check_system = cs.check_id
+            where cs.check_type = 1) as ch
       where rh.hospital = ch.hospital;
 
       --删除check_id为空的数据
-      delete from report_hospital where check_id is null;
-      delete from report_hospital_history where check_id is null;
+      delete
+      from report_hospital
+      where check_id is null;
+      delete
+      from report_hospital_history
+      where check_id is null;
 
       --删除report_hospital原有的主键约束
-      alter table report_hospital drop constraint report_hospital_pkey;
+      alter table report_hospital
+        drop constraint report_hospital_pkey;
       --建立新的联合主键约束
-      alter table report_hospital add primary key ("hospital", "check_id");
+      alter table report_hospital
+        add primary key ("hospital", "check_id");
 
       --删除"report_hospital_hospital"原有的主键约束
-      alter table report_hospital_history drop constraint report_hospital_history_pkey;
+      alter table report_hospital_history
+        drop constraint report_hospital_history_pkey;
       --建立新的联合主键约束
-      alter table report_hospital_history add primary key ("date", "hospital", "check_id");
+      alter table report_hospital_history
+        add primary key ("date", "hospital", "check_id");
     `);
   }
 
