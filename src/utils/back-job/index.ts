a7f39fd96cf12e4e9d88229f4ad7694b62464e20
吴成浩 <wuchenghao@bjknrt.com>
2020-12-10 18:36:59 +0800

    socket初始化方法、back-job任务创建方法

--- src/utils/back-job/index.ts
+++ src/utils/back-job/index.ts
@@ -1,29 +1,94 @@
 import {Worker} from 'worker_threads';
 import * as path from 'path';
+import {Server, Socket} from 'socket.io';
+import {Context} from '../../api/context';
+import {KatoCommonError} from 'kato-server';
+
+const jobType: Array<string> = ['reportCheck'];
+
+type Job = {
+  id: string;
+  userId: string;
+  job: string;
+  title: string;
+  status: 'running' | 'success' | 'error';
+  startTime: Date;
+  endTime?: Date;
+  result?: any;
+};
+
+//客户端集合
+const clients: Array<{id: string; socket: Socket}> = [];
+//任务集合
+const jobs = new Map<string, Job>();
+
+/***
+ * 后台任务初始化
+ * @param app
+ */
+export async function init(app) {
+  //设置socket
+  const io = new Server(app, {path: '/back-job'});
+  io.on('connection', (socket: Socket) => {
+    const id: string = socket.handshake?.query?.id;
+
+    if (!id) {
+      socket.disconnect(true);
+      return;
+    }
+    const current = clients.find(it => it.id === id);
+    if (!current) clients.push({id, socket});
+    //更新当前用户的后台任务事件
+    jobs.forEach(value => {
+      if (value.userId === id) socket.emit('update', value);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('后端断开');
+      const index = clients.findIndex(it => it.id === id);
+      if (index > -1) clients.splice(index, 1);
+    });
+  });
+}
 
 /***
  * 创建后台worker
- * @param job : 任务的名称
- * @param data : 所需参数
+ * @params job : 任务的类型
+ * @params title: 任务名称
+ * @params data : 所需参数
  */
-export default function createBackJob(job: string, data?: object) {
+export async function createBackJob(job: string, title: string, data?: object) {
   try {
-    const work = new Worker(path.join(__dirname, './worker.js'), {
-      workerData: job
-    });
-    const workData = JSON.stringify({job, ...data});
+    if (jobType.indexOf(job) < 0) throw new KatoCommonError('任务不存在');
 
-    //发布后台任务
-    work.postMessage(workData);
+    const userId = Context.current.user.id;
+    const backJob: Job = {
+      id: userId + job, //同一个用户同一个类型的任务始终保持一个
+      userId,
+      job,
+      title,
+      status: 'running',
+      startTime: new Date()
+    };
+
+    jobs.set(backJob.id, backJob);
+
+    const work = new Worker(path.join(__dirname, './worker.js'), {
+      workerData: {job, ...data}
+    });
+    const client = clients.find(it => it.id === userId);
+
     //监听后台任务的结果
     work.on('message', data => {
+      // socket.io
       console.log('main等到的消息', data);
+      backJob.result = data;
+      backJob.status = 'success';
+      backJob.endTime = new Date();
+      if (client) client.socket.emit('update', backJob);
     });
-    return new Promise(resolve => {
-      work.on('message', val => {
-        resolve(val);
-      });
-    });
+    if (client) client.socket.emit('update', backJob);
+    return backJob;
   } catch (e) {
     console.log('发生错误', e);
   }
