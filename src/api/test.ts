be392802c38eb38f33e634d42cf5d3cefcf5b2bd
吴成浩 <wuchenghao@bjknrt.com>
2020-12-11 16:14:40 +0800

    还原test.ts的测试代码

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,16 +1,5 @@
-import {Worker} from 'worker_threads';
-
 export default class Test {
   test() {
     return new Date();
-  }
-
-  worker(n) {
-    const work = new Worker('./src/utils/back-work.js', {workerData: n});
-    return new Promise(resolve => {
-      work.on('message', val => {
-        resolve(val);
-      });
-    });
   }
 }
