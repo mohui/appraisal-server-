2c753dae7710eaed3124fc03f0ee59798fe449ac
谢高军 <xiegaojun@bjknrt.com>
2020-12-28 15:36:33 +0800

    添加loading

--- ui/web/views/appraisal-result/institutions/index.vue
+++ ui/web/views/appraisal-result/institutions/index.vue
@@ -726,7 +726,10 @@
             </el-card>
           </el-col>
           <el-col :span="12" :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
-            <el-card shadow="hover">
+            <el-card
+              shadow="hover"
+              v-loading="$asyncComputed.workPointsProjectServerData.updating"
+            >
               <p style="color:#1096d0; font-size:20px; font-weight:500;">
                 工分项目
               </p>
