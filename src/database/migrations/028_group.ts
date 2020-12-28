407f2148c4ce894ef1cfd192f140d53448a498e8
吴成浩 <wuchenghao@bjknrt.com>
2020-12-28 18:34:33 +0800

    修改28号迁移脚本

--- src/database/migrations/028_group.ts
+++ src/database/migrations/028_group.ts
@@ -151,6 +151,28 @@ export class GroupMigration implements IMigration {
       COMMENT ON COLUMN "report_area_history"."workPoint" IS '参与校正工分';
       COMMENT ON COLUMN "report_area_history"."score" IS '得分';
       COMMENT ON COLUMN "report_area_history"."budget" IS '分配金额';
+
+      --手动打分备注和历史表
+      CREATE TABLE IF NOT EXISTS "manual_score_history"
+      (
+        "id"         UUID,
+        "rule"       UUID                     NOT NULL REFERENCES "check_rule" ("rule_id") ON DELETE NO ACTION ON UPDATE CASCADE,
+        "code"       varchar(36)              NOT NULL REFERENCES "area" ("code") ON DELETE NO ACTION ON UPDATE CASCADE,
+        "score"      FLOAT        DEFAULT 0,
+        "remark"     VARCHAR(255) DEFAULT '',
+        "creator"    UUID REFERENCES "user" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+        "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+        "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+        PRIMARY KEY ("id")
+      );
+      COMMENT ON COLUMN "manual_score_history"."rule" IS '考核小项id';
+      COMMENT ON COLUMN "manual_score_history"."code" IS '地区code或者机构id';
+      COMMENT ON COLUMN "manual_score_history"."score" IS '手动打分分数';
+      COMMENT ON COLUMN "manual_score_history"."remark" IS '备注信息';
+      COMMENT ON COLUMN "manual_score_history"."creator" IS '打分者';
+      COMMENT ON COLUMN "manual_score_history"."created_at" IS '创建时间';
+      COMMENT ON COLUMN "manual_score_history"."updated_at" IS '更新时间';
+
     `);
     // 处理数据
 
