1ca1a69936b590f94534c9c72a0dec123052275d
戴凯杰 <daikaijie@bjknrt.com>
2021-01-04 10:26:10 +0800

    添加获取子级的接口

--- src/api/group.ts
+++ src/api/group.ts
@@ -326,4 +326,16 @@ export default class Group {
       };
     });
   }
+
+  /**
+   * 获取当前code的子级
+   *
+   * @param code 地区code
+   */
+  async children(code) {
+    if (!code) {
+      code = Context.current.user.code;
+    }
+    return AreaModel.findAll({where: {parent: code}});
+  }
 }
