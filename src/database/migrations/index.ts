bb626db76983f6121deb7703f55f83f50445f291
戴凯杰 <daikaijie@bjknrt.com>
2021-01-05 23:55:04 +0800

    地区细则得分表添加指标解释数组字段

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -29,6 +29,7 @@ import {ProfileTagsDetailMigration} from './026_profile_tags_permission';
 import {ScoreRemarkHistoryMigration} from './027_score_remark_history';
 import {GroupMigration} from './028_group';
 import {BasicTagDelYearMigration} from './029_basic_tag';
+import {RuleDetailsMigration} from './030_rule_details';
 
 //定义好的数据迁移任务放入到数组中,顺序任意
 export const migrations: IMigration[] = [
@@ -61,5 +62,6 @@ export const migrations: IMigration[] = [
   new ProfileTagsDetailMigration(),
   new ScoreRemarkHistoryMigration(),
   new GroupMigration(),
-  new BasicTagDelYearMigration()
+  new BasicTagDelYearMigration(),
+  new RuleDetailsMigration()
 ];
