8484c1bd856284914eb7ffca1312f4c9bc7fb4a6
戴凯杰 <daikaijie@bjknrt.com>
2021-01-08 17:12:41 +0800

    完成 健康教育讲座次数合格率基础数据维护 迁移任务

--- src/app.ts
+++ src/app.ts
@@ -82,7 +82,7 @@ export class Application {
     const migrate = new Migrater(this.appDB);
     migrations.forEach(m => migrate.addMigration(m));
     if (process.env.NODE_ENV === 'production') {
-      await migrate.migrate(31);
+      await migrate.migrate(32);
     }
   }
 
