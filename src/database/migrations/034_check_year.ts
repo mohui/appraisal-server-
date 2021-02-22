8497bab03c051e0bbb9dae8c353c5c925c25188e
王贺辉 <wanghehui@bjknrt.com>
2021-02-22 14:53:02 +0800

    修改check_system字段check_year为int类型

--- src/database/migrations/034_check_year.ts
+++ src/database/migrations/034_check_year.ts
@@ -1,14 +1,14 @@
 import {IMigration} from '../migrater';
 import {ExtendedSequelize} from '../client';
-import {BasicTagUsages} from '../../../common/rule-score';
-import {v4 as uuid} from 'uuid';
 
 export class CheckYearMigration implements IMigration {
   name = '考核年份改为int型';
   version = 34;
 
   async up(client: ExtendedSequelize): Promise<void> {
-    return;
+    await client.execute(
+      `ALTER TABLE check_system ALTER COLUMN check_year TYPE integer USING (check_year::integer);`
+    );
   }
 
   async down(client: ExtendedSequelize, err?: Error): Promise<void> {
