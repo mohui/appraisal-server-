bbc7f1d5d1cb28c622203f4e91f8f0e962c28853
谢高军 <xiegaojun@bjknrt.com>
2021-02-25 14:22:16 +0800

    产后42天健康检查记录表详情参数修改

--- ui/web/views/patient/index.vue
+++ ui/web/views/patient/index.vue
@@ -762,7 +762,7 @@ export default {
       } else if (name === '产后42天健康检查记录表') {
         //产后42天健康检查记录表
         routerName = 'record-postpartum-42-days-check';
-        code = record.newlydiagnosedcode;
+        code = record.examineno;
       }
       if (routerName && code) {
         this.$router.push({
