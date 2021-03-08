acd67988ae1f4431ca8a4e20b1d0f527ca4e387e
谢高军 <xiegaojun@bjknrt.com>
2021-03-08 15:04:18 +0800

    孕产妇健康记录列表调整

--- ui/web/views/patient/index.vue
+++ ui/web/views/patient/index.vue
@@ -591,13 +591,13 @@
                         </div>
                         <p>
                           <span v-if="it.type === 'newlyDiagnosed'">
-                            年龄：{{ record.age }}
+                            年龄：{{ record.age }} 体重：{{ record.weight }}kg
                           </span>
-                          体重：{{ record.weight }}kg
                           <span
                             v-if="
                               it.type === 'prenatalCare' ||
-                                it.type === 'examine42thDay'
+                                it.type === 'examine42thDay' ||
+                                it.type === 'maternalVisits'
                             "
                           >
                             医生姓名：{{ record.doctor }}
