8d130be128a1d4faefbc1736ddcd1ac370b5162e
王贺辉 <wanghehui@bjknrt.com>
2021-04-27 17:39:19 +0800

    去掉模块查

--- src/api/group/audit_log.ts
+++ src/api/group/audit_log.ts
@@ -3,10 +3,8 @@ import {sql as sqlRender} from '../../database/template';
 import {Context} from '../context';
 
 export default class AuditLog {
-  async list(start, end, checkId, name, module, method, pageNo, pageSize) {
+  async list(start, end, checkId, name, pageNo, pageSize) {
     if (name) name = `%${name}%`;
-    if (module) module = `%${module}%`;
-    if (method) method = `%${method}%`;
     const [sql, params] = sqlRender(
       `
         select *
@@ -21,25 +19,15 @@ export default class AuditLog {
         {{#if checkId}}
             AND extra ->> 'checkId' = {{? checkId}}
         {{/if}}
-        {{#if module}}
-            AND "module" like {{? module}}
-        {{/if}}
-        {{#if method}}
-            AND "method" like {{? method}}
-        {{/if}}
         order by created_at desc
       `,
       {
         name,
-        module,
-        method,
         checkId,
         start: start,
         end: end
       }
     );
-    console.log(sql);
-    console.log(params);
     return await appDB.page(sql, pageNo ?? 1, pageSize ?? 10, ...params);
   }
   async checkSystems() {
