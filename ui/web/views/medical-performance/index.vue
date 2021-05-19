14446a7730f4387407b3bf8790ee6a4951d03c8c
谢高军 <xiegaojun@bjknrt.com>
2021-05-19 11:30:05 +0800

    医生工分/质量系数柱状图

--- ui/web/views/medical-performance/index.vue
+++ ui/web/views/medical-performance/index.vue
@@ -34,7 +34,10 @@
           </el-col>
           <el-col :span="16" :xs="24" :sm="16" :md="16" :lg="16" :xl="16">
             <el-card shadow="hover">
-              员工列表
+              <div
+                id="doctorPerformanceBar"
+                :style="{width: '100%', height: '300px'}"
+              ></div>
             </el-card>
           </el-col>
         </el-row>
@@ -56,16 +59,111 @@ export default {
       dataSource: {
         beforeCorrectionWorkPoint: 30000,
         afterCorrectionWorkPoint: 25000,
-        rate: 56
+        rate: 56,
+        doctorData: [
+          {name: '医生1', score: 30, rate: 0.8},
+          {name: '医生2', score: 70, rate: 0.5},
+          {name: '医生3', score: 50, rate: 0.7},
+          {name: '医生4', score: 60, rate: 0.6},
+          {name: '医生5', score: 90, rate: 0.6},
+          {name: '医生6', score: 50, rate: 0.9},
+          {name: '医生7', score: 70, rate: 0.4}
+        ]
       }
     };
   },
   components: {
     twoCardCircle
   },
-
   directives: {
     sticky: VueSticky
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = this.$echarts.init(
+        document.getElementById('doctorPerformanceBar')
+      );
+      let option;
+      const colors = ['#409eff', '#ea9d42'];
+      option = {
+        color: colors,
+
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          right: '20%'
+        },
+        legend: {
+          data: ['人员得分', '质量系数']
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: {
+              alignWithLabel: true
+            },
+            data: this.dataSource.doctorData.map(it => it.name)
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            name: '人员得分',
+            min: 0,
+            position: 'right',
+            axisLine: {
+              show: true,
+              lineStyle: {
+                color: colors[0]
+              }
+            },
+            axisLabel: {
+              formatter: '{value} 分'
+            }
+          },
+          {
+            type: 'value',
+            name: '质量系数',
+            min: 0,
+            max: 1,
+            axisLine: {
+              show: true,
+              lineStyle: {
+                color: colors[1]
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
+            }
+          }
+        ],
+        series: [
+          {
+            name: '人员得分',
+            type: 'bar',
+            yAxisIndex: 0,
+            data: this.dataSource.doctorData.map(it => it.score)
+          },
+          {
+            name: '质量系数',
+            type: 'bar',
+            yAxisIndex: 1,
+            data: this.dataSource.doctorData.map(it => it.rate)
+          }
+        ]
+      };
+
+      // 绘制图表
+      myChart.setOption(option);
+    }
   }
 };
 </script>
