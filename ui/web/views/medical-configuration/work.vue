61b6d160805d43f128e307e7ccb655903f71fbe0
吴成浩 <wuchenghao@bjknrt.com>
2021-05-19 17:27:17 +0800

    工分项的配置新增,修改,删除功能

--- ui/web/views/medical-configuration/work.vue
+++ ui/web/views/medical-configuration/work.vue
@@ -14,6 +14,9 @@
       <div slot="header" class="work-header">
         <span>工分项配置</span>
         <div>
+          <el-button size="mini" type="primary" @click="addWorkVisible = true"
+            >新增工分项</el-button
+          >
           <el-button
             size="mini"
             type="text"
@@ -47,13 +50,107 @@
         </el-form>
       </kn-collapse>
       <el-table
+        v-loading="tableLoading"
         stripe
         size="small"
-        :data="[]"
+        :data="tableData"
         height="100%"
         style="flex-grow: 1;"
         :header-cell-style="{background: '#F3F4F7', color: '#555'}"
       >
+        <el-table-column prop="index" label="序号"></el-table-column>
+        <el-table-column prop="work" label="工分项">
+          <template slot-scope="{row}">
+            <div v-if="!row.isEdit">{{ row.work }}</div>
+            <div v-else>
+              <el-input v-model="tempRow.work" size="mini"> </el-input>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="type" label="打分类型">
+          <template slot-scope="{row}">
+            <div v-if="!row.isEdit">{{ row.scoreType }}</div>
+            <div v-else>
+              <el-select v-model="tempRow.scoreType" size="mini">
+                <el-option label="手动打分" value="手动打分"></el-option>
+                <el-option label="自动打分" value="自动打分"></el-option>
+              </el-select>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="project" label="关联项目">
+          <template slot-scope="{row}">
+            <div v-if="!row.isEdit">{{ row.projects }}</div>
+            <div v-else>
+              <el-select
+                v-model="tempRow.projects"
+                size="mini"
+                multiple
+                collapse-tags
+              >
+                <el-option
+                  v-for="work in projectList"
+                  :key="work.value"
+                  :label="work.name"
+                  :value="work.value"
+                ></el-option>
+              </el-select>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="createdAt" label="创建时间"></el-table-column>
+        <el-table-column prop="" label="操作">
+          <template slot-scope="{row}">
+            <el-tooltip v-show="!row.isEdit" content="编辑" :enterable="false">
+              <el-button
+                type="primary"
+                icon="el-icon-edit"
+                circle
+                size="mini"
+                @click="editRow(row.index)"
+              >
+              </el-button>
+            </el-tooltip>
+            <el-tooltip
+              v-show="row.isEdit"
+              content="提交修改"
+              :enterable="false"
+            >
+              <el-button
+                type="success"
+                icon="el-icon-check"
+                circle
+                size="mini"
+                @click="submitEdit(row.index, tempRow)"
+              >
+              </el-button>
+            </el-tooltip>
+            <el-tooltip
+              v-show="row.isEdit"
+              content="取消修改"
+              :enterable="false"
+            >
+              <el-button
+                type="default"
+                icon="el-icon-close"
+                circle
+                size="mini"
+                @click="cancelEdit(row.index)"
+              >
+              </el-button>
+            </el-tooltip>
+            <el-tooltip v-show="!row.isEdit" content="删除" :enterable="false">
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                circle
+                size="mini"
+                @click="removeRow(row)"
+              >
+              </el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
       <el-pagination
         background
@@ -76,11 +173,54 @@
       >
       </el-pagination>
     </el-card>
+    <el-dialog
+      title="新建配置"
+      :visible.sync="addWorkVisible"
+      :width="$settings.isMobile ? '99%' : '50%'"
+      :before-close="resetConfig"
+    >
+      <el-form
+        ref="workForm"
+        :model="newWork"
+        :rules="workRules"
+        label-position="right"
+        label-width="120px"
+      >
+        <el-form-item label="工分项" prop="work">
+          <el-input v-model="newWork.work"> </el-input>
+        </el-form-item>
+        <el-form-item label="关联项目" prop="projects">
+          <el-select v-model="newWork.projects" multiple>
+            <el-option
+              v-for="p in projectList"
+              :key="p.value"
+              :label="p.name"
+              :value="p.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="打分方式" prop="scoreType">
+          <el-radio v-model="newWork.scoreType" label="自动打分"
+            >自动打分</el-radio
+          >
+          <el-radio v-model="newWork.scoreType" label="手动打分"
+            >手动打分</el-radio
+          >
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resetConfig()">取 消</el-button>
+        <el-button type="primary" @click="submit()">
+          确 定
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import {Permission} from '../../../../common/permission.ts';
+import dayjs from 'dayjs';
 
 export default {
   name: 'User',
@@ -89,15 +229,137 @@ export default {
       isCollapsed: !!this.$settings.isMobile,
       permission: Permission,
       searchForm: {
+        work: '',
+        scoreType: '',
+        projects: [],
         pageSize: 20,
         pageNo: 1
-      }
+      },
+      newWork: {
+        work: '',
+        scoreType: '',
+        projects: []
+      },
+      addWorkVisible: false,
+      tempRow: '',
+      workRules: {
+        work: [{required: true, message: '填写工分项', trigger: 'change'}],
+        projects: [
+          {required: true, message: '选择关联项目', trigger: 'change'}
+        ],
+        scoreType: [
+          {required: true, message: '选择打分方式', trigger: 'change'}
+        ]
+      },
+      tableLoading: false
     };
   },
-  computed: {},
+  computed: {
+    tableData() {
+      return this.serverData.rows.map(d => ({...d, isEdit: false}));
+    },
+    projectList() {
+      return this.serverProjectData;
+    }
+  },
   watch: {},
-  asyncComputed: {},
-  methods: {}
+  asyncComputed: {
+    serverData: {
+      async get() {
+        let data = [];
+        this.tableLoading = true;
+        const {scoreType, score, work} = this.searchForm;
+        console.log(scoreType, score, work);
+        try {
+          await new Promise(resolve =>
+            setTimeout(() => {
+              for (let i = 0; i < 10; i++) {
+                data.push({
+                  index: i + 1,
+                  work: `工分项${i}`,
+                  scoreType: '自动打分',
+                  projects: [`项目${i + 1}`],
+                  createdAt: '2021-05-18 11:23:21'
+                });
+              }
+              resolve();
+            }, 1000)
+          );
+          return {
+            counts: 10,
+            rows: data
+          };
+        } catch (e) {
+          console.error(e.message);
+        } finally {
+          this.tableLoading = false;
+        }
+      },
+      default: {counts: 0, rows: []}
+    },
+    serverProjectData: {
+      async get() {
+        await new Promise(resolve => setTimeout(() => resolve(), 600));
+        let i = 0;
+        let data = [];
+        for (; i < 10; i++) {
+          data.push({name: `项目${i}`, value: `项目${i}`});
+        }
+        return data;
+      },
+      default: []
+    }
+  },
+  methods: {
+    async submit() {
+      try {
+        const valid = await this.$refs['workForm'].validate();
+        if (valid) {
+          this.$set(this.serverData.rows, this.tableData.length, {
+            index: this.tableData.length + 1,
+            work: this.newWork.work,
+            scoreType: this.newWork.scoreType,
+            projects: this.newWork.projects,
+            row: false,
+            createdAt: dayjs().format('YYYY-MM-DD HH:mm:ss')
+          });
+          this.resetConfig();
+        }
+      } catch (e) {
+        console.error(e);
+        if (e) this.$message.error(e.message);
+      }
+    },
+    editRow(index) {
+      if (this.tempRow) {
+        this.$message.warning('已有其他数据正在编辑');
+        return;
+      }
+      this.tableData[index - 1].isEdit = !this.tableData[index - 1].isEdit;
+      this.tempRow = Object.assign({}, this.tableData[index - 1]);
+    },
+    cancelEdit(index) {
+      this.tableData[index - 1].isEdit = !this.tableData[index - 1].isEdit;
+      this.tempRow = '';
+    },
+    async submitEdit(index, tempRow) {
+      tempRow.isEdit = !tempRow.isEdit;
+      this.$set(this.serverData.rows, index - 1, tempRow);
+      this.$message.success('修改成功');
+      this.tempRow = '';
+    },
+    async removeRow(row) {
+      this.tableData.splice(
+        this.tableData.findIndex(d => d.index === row.index),
+        1
+      );
+      this.$message.success('删除成功');
+    },
+    resetConfig() {
+      this.$refs['workForm'].resetFields();
+      this.addWorkVisible = false;
+    }
+  }
 };
 </script>
 
