63ee6df93f306fb7cbabcc387702d22610b043c9
王贺辉 <wanghehui@bjknrt.com>
2021-05-21 16:58:43 +0800

    添加员工, 员工列表

--- src/api/his/his_staff.ts
+++ src/api/his/his_staff.ts
@@ -2,19 +2,24 @@ import {appDB, originalDB} from '../../app';
 import {v4 as uuid} from 'uuid';
 import * as dayjs from 'dayjs';
 import {Context} from '../context';
+import {KatoRuntimeError, should, validate} from 'kato-server';
 
 export default class HisStaff {
   /**
    * 查询his员工
    */
   async listHisStaffs() {
-    // Context.current.user.hospitals;
-    //
-    // const hisStaffs = await originalDB.execute(
-    //   `select id, name, hospital from his_staff where hospital = ?`,
-    //   hospital
-    // );
-    // return hisStaffs;
+    if (
+      Context.current.user.hospitals &&
+      Context.current.user.hospitals.length > 1
+    )
+      throw new KatoRuntimeError(`没有查询his员工权限`);
+    const hospital = Context.current.user.hospitals[0]['id'];
+
+    return await originalDB.execute(
+      `select id, name, hospital from his_staff where hospital = ?`,
+      hospital
+    );
   }
 
   /**
@@ -24,9 +29,37 @@ export default class HisStaff {
    * @param staff
    * @param account
    * @param password
-   * @param username
+   * @param name
    */
+  @validate(
+    should
+      .string()
+      .required()
+      .description('所属医院'),
+    should
+      .string()
+      .allow(null, '')
+      .description('绑定his员工id'),
+    should
+      .string()
+      .required()
+      .description('登录名'),
+    should
+      .string()
+      .required()
+      .description('密码'),
+    should
+      .string()
+      .required()
+      .description('名称')
+  )
   async add(hospital, staff, account, password, name) {
+    // 查询his员工是否已经被绑定
+    const accountOne = await appDB.execute(
+      `select * from staff where staff = ?`,
+      staff
+    );
+    if (accountOne.length > 0) throw new KatoRuntimeError(`his员工已经存在`);
     return await appDB.execute(
       `insert into
             staff(
