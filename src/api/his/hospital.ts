c45482e82ce72957963811ddc98f30d95ac722ce
戴凯杰 <daikaijie@bjknrt.com>
2021-05-22 17:25:36 +0800

    调整机构结算接口

--- src/api/his/hospital.ts
+++ src/api/his/hospital.ts
@@ -8,34 +8,12 @@ import {should, validate} from 'kato-server';
  */
 export default class HisHospital {
   /**
-   * 获取当前机构是否结算
+   * 结算指定月份
    *
    * @param month 月份
    */
   @validate(should.date().required())
-  async getSettle(month) {
-    const hospital = await getHospital();
-    const date = dayjs(month)
-      .startOf('M')
-      .toDate();
-    return (
-      (
-        await appDB.execute(
-          `select settle from his_hospital_settle where hospital = ? and month = ?`,
-          hospital,
-          date
-        )
-      )[0]?.settle ?? false
-    );
-  }
-
-  /**
-   * 结算
-   *
-   * @param month 月份
-   */
-  @validate(should.date().required())
-  async setSettle(month) {
+  async settle(month) {
     const hospital = await getHospital();
     const date = dayjs(month)
       .startOf('M')
