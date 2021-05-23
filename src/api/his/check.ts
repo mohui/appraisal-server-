f247f680f82dc3b2ee422f8fc9db3c16b2833924
王贺辉 <wanghehui@bjknrt.com>
2021-05-23 19:57:22 +0800

    方案列表

--- src/api/his/check.ts
+++ src/api/his/check.ts
@@ -7,33 +7,92 @@ import {HisWorkMethod, HisWorkSource} from '../../../common/his';
 
 export default class HisCheck {
   async list() {
+    // 获取所属机构
     const hospital = await getHospital();
+    // 获取考核方案名称
     const systemList = await appDB.execute(
       `select
               system.id,
               system.hospital,
               system.name,
-              mapping.staff
+              mapping.staff,
+              staff.account,
+              staff.name "staffName"
         from his_check_system system
         left join his_staff_check_mapping mapping on system.id = mapping."check"
+        left join staff on mapping.staff = staff.id
         where system.hospital = ?
       `,
       hospital
     );
+    // 获取员工id
+    const staffs = systemList.map(it => it.staff);
+
+    // 根据员工id获取负责项目
+    const workItems = await appDB.execute(
+      `select item.id, item.name, mapping.staff
+            from his_staff_work_item_mapping mapping
+            left join his_work_item item on mapping.item = item.id
+            where mapping.staff in (${staffs.map(() => '?')})`,
+      ...staffs
+    );
+    // 获取医疗方案分值
+    const systemScores = await appDB.execute(
+      `select checkRule."check" "checkId", sum(checkRule.score) score
+            from his_check_rule checkRule
+            left join his_check_system system on checkRule."check" = system.id
+            where system.hospital = ?
+            group by checkRule."check"`,
+      hospital
+    );
+
+    const systemList1 = systemList.map(it => {
+      const workItemObj = workItems.find(workIt => workIt.staff === it.staff);
+      return {
+        ...it,
+        workId: workItemObj?.id,
+        workName: workItemObj?.name
+      };
+    });
+
+    // 获取考核员工
     const returnList = [];
-    for (const it of systemList) {
-      const findIndex = returnList.find(find => find?.id === it.id);
+    for (const it of systemList1) {
+      // 查找工分项目
+      const scoreObj = systemScores.find(item => item.checkId === it.id);
+
+      // 查找考核得分
+      const findIndex = returnList.find(find => find.id === it.id);
       if (findIndex) {
         findIndex.staffs.push({
-          staff: it.staff
+          staff: it.staff,
+          account: it.account,
+          name: it.staffName
         });
+        const index = findIndex.item.find(itemIt => itemIt.id === it.workId);
+        // 如果没有查找到工分项目,并且工分项目存在
+        if (!index && it?.workId) {
+          findIndex.item.push({
+            id: it.workId,
+            name: it.workName
+          });
+        }
       } else {
         returnList.push({
           id: it.id,
           name: it.name,
+          score: scoreObj?.score,
           staffs: [
             {
-              staff: it.staff
+              staff: it.staff,
+              account: it.account,
+              name: it.staffName
+            }
+          ],
+          item: [
+            {
+              id: it.workId,
+              name: it.workName
             }
           ]
         });
