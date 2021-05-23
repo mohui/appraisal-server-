800e575dbfd173ace851b8140f1972dbd6633c56
吴成浩 <wuchenghao@bjknrt.com>
2021-05-23 16:19:49 +0800

    员工删除接口对接

--- ui/web/views/medical-configuration/member-his.vue
+++ ui/web/views/medical-configuration/member-his.vue
@@ -418,18 +418,19 @@ export default {
       });
     },
     //删除用户
-    async delUser({$index}) {
+    async delUser(row) {
       try {
         await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         });
-        this.userList.splice($index, 1);
+        await this.$api.HisStaff.delete(row.id);
         this.$message({
           type: 'success',
           message: '删除成功!'
         });
+        this.$asyncComputed.listMember.update();
       } catch (e) {
         console.log(e);
         // this.$message.error(e);
