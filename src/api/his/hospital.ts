c866ae676e92df3af52b50ef6578a9a62bb736d0
戴凯杰 <daikaijie@bjknrt.com>
2021-05-24 16:28:21 +0800

    添加 机构考核结果概览 接口

--- src/api/his/hospital.ts
+++ src/api/his/hospital.ts
@@ -1,7 +1,8 @@
 import {getHospital} from './his_staff';
 import {appDB} from '../../app';
 import * as dayjs from 'dayjs';
-import {should, validate} from 'kato-server';
+import {KatoRuntimeError, should, validate} from 'kato-server';
+import {monthToRange} from './manual';
 
 /**
  * 机构模块
@@ -30,5 +31,81 @@ export default class HisHospital {
       hospital,
       date
     );
+  }
+
+  /**
+   * 考核结果概览
+   *
+   * @param month 月份
+   * @return {
+   *   id: 机构id
+   *   name: 机构名称
+   *   settle: 是否结算
+   *   date: 考核时间
+   *   originalScore: 校正前工分
+   *   correctScore: 校正后工分
+   * }
+   */
+  @validate(should.date().required())
+  async overview(month) {
+    const hospital = await getHospital();
+    //查询机构
+    // language=PostgreSQL
+    const hospitalModel: {id: string; name: string} = (
+      await appDB.execute(
+        `
+          select code as id, name
+          from area
+          where code = ?
+        `,
+        hospital
+      )
+    )[0];
+    if (!hospitalModel) throw new KatoRuntimeError(`该机构不存在`);
+    const {start, end} = monthToRange(month);
+    //查询是否结算
+    // language=PostgreSQL
+    const settle =
+      (
+        await appDB.execute(
+          `
+            select settle
+            from his_hospital_settle
+            where hospital = ?
+              and month = ?
+          `,
+          hospitalModel.id,
+          start
+        )
+      )[0]?.settle ?? false;
+    //查询校正前总工分
+    // language=PostgreSQL
+    const originalScore = Number(
+      (
+        await appDB.execute(
+          `
+            select sum(his_staff_work_score_detail.score) as score
+            from his_staff_work_score_detail
+                   inner join staff on his_staff_work_score_detail.staff = staff.id
+            where staff.hospital = ?
+              and his_staff_work_score_detail.date >= ?
+              and his_staff_work_score_detail.date < ?
+          `,
+          hospitalModel.id,
+          start,
+          end
+        )
+      )[0]?.score ?? 0
+    );
+    //TODO: 查询校正后工分
+
+    return {
+      id: hospitalModel.id,
+      name: hospitalModel.name,
+      settle: settle,
+      date: month,
+      originalScore,
+      correctScore: null
+    };
   }
 }
