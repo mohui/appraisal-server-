a1d7bff26e36c7ed6bb96899e2af26b555a380ad
吴成浩 <wuchenghao@bjknrt.com>
2021-05-26 12:19:06 +0800

    对接员工/工分项配置列表删除接口

--- ui/web/views/medical-configuration/index.vue
+++ ui/web/views/medical-configuration/index.vue
@@ -399,11 +399,18 @@ export default {
       this.tempRow = '';
     },
     async removeRow(row) {
-      this.tableData.splice(
-        this.tableData.findIndex(d => d.index === row.index),
-        1
-      );
-      this.$message.success('删除成功');
+      try {
+        await this.$confirm('确定删除该配置?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        });
+        await this.$api.HisWorkItem.delStaffWorkItemMapping(row.id);
+        this.$message.success('删除成功');
+        this.$asyncComputed.serverData.update();
+      } catch (e) {
+        console.log(e);
+      }
     },
     resetConfig() {
       this.$refs['configForm'].resetFields();
