b7c80f0d283efb0ac5a4b7e864c86d8067414031
戴凯杰 <daikaijie@bjknrt.com>
2021-05-27 14:02:56 +0800

    抽离公共的月份参数校验器

--- common/his.ts
+++ common/his.ts
@@ -1,3 +1,30 @@
+import * as dayjs from 'dayjs';
+import {should} from 'kato-server';
+
+/**
+ * 获取时间区间
+ *
+ * @return {
+ *   start: 默认开始时间 2021年5月
+ *   end: 当前时间点
+ * }
+ */
+export function getTimeRange(): {start: Date; end: Date} {
+  return {
+    start: dayjs('2021-05-01').toDate(),
+    end: dayjs().toDate()
+  };
+}
+
+/**
+ * 月份参数校验
+ */
+export const monthValid = should
+  .date()
+  .min(getTimeRange().start)
+  .max(getTimeRange().end)
+  .required();
+
 /**
  * 手工数据输入方式
  */
