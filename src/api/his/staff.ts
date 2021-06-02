811f209eb2e0454f57f43046cc080b11e5beb3df
戴凯杰 <daikaijie@bjknrt.com>
2021-06-02 17:16:00 +0800

    返回值增加 extra 字段

--- src/api/his/staff.ts
+++ src/api/his/staff.ts
@@ -105,7 +105,7 @@ export default class HisStaff {
       sex: hisModel.sex,
       phone: hisModel.phone,
       birth: hisModel.birth,
-      extra: score,
+      extra: score ?? null,
       settle
     };
   }
