f54824fe65dc771cd22fb66016cfa690c9b44572
吴成浩 <wuchenghao@bjknrt.com>
2021-06-05 15:33:34 +0800

    完善decimal的使用方式

--- ui/web/views/medical-configuration/member.vue
+++ ui/web/views/medical-configuration/member.vue
@@ -369,7 +369,7 @@ export default {
                 id: it.id,
                 member: it.sources.map(s => s.name),
                 staffs: it.sources.map(s => s.id),
-                rate: new Decimal(Number(it.rate)).mul(100).toNumber()
+                rate: new Decimal(it.rate).mul(100).toNumber()
               }))
               .sort(a => {
                 //把本人的数据排到第一个;
