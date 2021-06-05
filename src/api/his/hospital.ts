12f317c3bd6d9242fc7bd71537599a1ded22ea58
戴凯杰 <daikaijie@bjknrt.com>
2021-06-05 14:43:11 +0800

    调整机构考核结果概览接口

--- src/api/his/hospital.ts
+++ src/api/his/hospital.ts
@@ -1,8 +1,8 @@
 import HisStaff from './staff';
 import {appDB} from '../../app';
-import * as dayjs from 'dayjs';
 import {KatoRuntimeError, validate} from 'kato-server';
-import {dateValid, getHospital, monthToRange} from './service';
+import {dateValid, getHospital, getSettle, monthToRange} from './service';
+import Decimal from 'decimal.js';
 
 /**
  * 机构模块
@@ -32,7 +32,7 @@ export default class HisHospital {
   }
 
   /**
-   * 考核结果概览
+   * 机构考核结果概览
    *
    * @param month 月份
    * @return {
@@ -60,55 +60,31 @@ export default class HisHospital {
       )
     )[0];
     if (!hospitalModel) throw new KatoRuntimeError(`该机构不存在`);
-    const {start, end} = monthToRange(month);
     //查询是否结算
-    // language=PostgreSQL
-    let settle =
-      (
-        await appDB.execute(
-          `
-            select settle
-            from his_hospital_settle
-            where hospital = ?
-              and month = ?
-          `,
-          hospitalModel.id,
-          start
-        )
-      )[0]?.settle ?? false;
-    //一个月前的结算状态, 默认已结算
-    if (dayjs().diff(month, 'M') > 1) {
-      settle = true;
-    }
-
-    //查询校正前总工分
-    // language=PostgreSQL
-    const originalScore = Number(
-      (
-        await appDB.execute(
-          `
-            select sum(his_staff_work_score_detail.score) as score
-            from his_staff_work_score_detail
-                   inner join staff on his_staff_work_score_detail.staff = staff.id
-            where staff.hospital = ?
-              and his_staff_work_score_detail.date >= ?
-              and his_staff_work_score_detail.date < ?
-          `,
-          hospitalModel.id,
-          start,
-          end
-        )
-      )[0]?.score ?? 0
+    const settle = await getSettle(hospital, month);
+    //查询员工工分结果
+    const staffs = await this.findStaffCheckList(month);
+    //累计工分
+    const scores = staffs.reduce(
+      (result, current) => {
+        //校正前工分
+        result.originalScore += current.score;
+        //校正后工分
+        if (current.rate && current.score) {
+          result.correctScore = new Decimal(current.score)
+            .mul(current.rate)
+            .toNumber();
+        }
+        return result;
+      },
+      {originalScore: 0, correctScore: 0}
     );
-    //TODO: 查询校正后工分
-
     return {
       id: hospitalModel.id,
       name: hospitalModel.name,
       settle: settle,
       date: month,
-      originalScore,
-      correctScore: null
+      ...scores
     };
   }
 
@@ -158,7 +134,9 @@ export default class HisHospital {
    * ]
    */
   @validate(dateValid)
-  async findStaffCheckList(month) {
+  async findStaffCheckList(
+    month
+  ): Promise<{id: string; name: string; score: number; rate: number}[]> {
     const staffApi = new HisStaff();
     const hospital = await getHospital();
     return Promise.all(
