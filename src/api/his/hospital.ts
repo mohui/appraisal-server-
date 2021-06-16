a24436176ff800051aa7867829a4a753131509cd
戴凯杰 <daikaijie@bjknrt.com>
2021-06-16 16:22:19 +0800

    格式化SQL

--- src/api/his/hospital.ts
+++ src/api/his/hospital.ts
@@ -145,7 +145,7 @@ export default class HisHospital {
             select id, name
             from staff
             where hospital = ?
-            and virtual = false
+              and virtual = false
             order by created_at
           `,
           hospital
