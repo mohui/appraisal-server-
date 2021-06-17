1354050850a98cf60d64cee4f74bd8891aaa262b
王贺辉 <wanghehui@bjknrt.com>
2021-06-17 16:48:00 +0800

    修复虚拟用户bug

--- ui/web/views/medical-configuration/member-his.vue
+++ ui/web/views/medical-configuration/member-his.vue
@@ -145,7 +145,7 @@
         </el-form-item>
         <el-form-item
           label="是否虚拟账户"
-          prop="his"
+          prop="virtual"
           :label-width="formLabelWidth"
         >
           <el-switch v-model="userForm.virtual"></el-switch>
@@ -210,7 +210,7 @@
         </el-form-item>
         <el-form-item
           label="是否虚拟账户"
-          prop="his"
+          prop="virtual"
           :label-width="formLabelWidth"
         >
           <el-switch v-model="userForm.virtual"></el-switch>
