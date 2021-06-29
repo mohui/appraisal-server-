54151d50a56cf4f1bca75c6fcc05e284b9d47711
戴凯杰 <daikaijie@bjknrt.com>
2021-06-29 15:30:29 +0800

    完成医学指南列表接口

--- src/api/guidelines.ts
+++ src/api/guidelines.ts
@@ -1,3 +1,8 @@
+import {sql as sqlRender} from '../database';
+import {originalDB, unifs} from '../app';
+import * as path from 'upath';
+import {should, validate} from 'kato-server';
+
 /**
  * 医学指南接口模块
  */
@@ -13,10 +18,49 @@ export default class Guidelines {
    * @return [{
    *   id: id
    *   name: 名称
-   *   url: 超链接
+   *   url?: 超链接
    * }]
    */
+  @validate(
+    should.object({
+      keyword: should.string().allow(null),
+      pageSize: should
+        .number()
+        .integer()
+        .positive()
+        .required(),
+      pageNo: should
+        .number()
+        .integer()
+        .positive()
+        .required()
+    })
+  )
   async list(params) {
-    return [];
+    //模糊查询参数
+    if (params.keyword) params.keyword = `%${params.keyword}%`;
+    //sql渲染
+    const sqlResult = sqlRender(
+      `select * from guideline {{#if keyword}} where name like {{? keyword}} {{/if}} order by updated_at`,
+      {
+        keyword: params.keyword
+      }
+    );
+    //分页查询
+    const result = await originalDB.page(
+      sqlResult[0],
+      params.pageNo,
+      params.pageSize,
+      ...sqlResult[1]
+    );
+    //组织返回值
+    for (const it of result.data) {
+      //文件是否存在
+      const exist = await unifs.isFileExist(it.name);
+      if (exist) it.url = await unifs.getExternalUrl(it.name);
+      //名称使用文件名, 不带后缀
+      it.name = path.parse(it.name).name;
+    }
+    return result;
   }
 }
