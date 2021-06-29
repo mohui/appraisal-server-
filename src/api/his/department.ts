a174149981f00299bdf9e6312f06a8e9168ebac3
王贺辉 <wanghehui@bjknrt.com>
2021-06-29 11:11:39 +0800

    科室接口的增改查, 删除最后做

---
+++ src/api/his/department.ts
@@ -0,0 +1,59 @@
+import {getHospital} from './service';
+import {appDB} from '../../app';
+import {v4 as uuid} from 'uuid';
+
+export default class HisDepartment {
+  async add(name) {
+    const hospital = await getHospital();
+    return appDB.transaction(async () => {
+      // language=PostgreSQL
+      return await appDB.execute(
+        `
+          insert into his_department(id, hospital, name, created_at, updated_at)
+          values (?, ?, ?, ?, ?)
+        `,
+        uuid(),
+        hospital,
+        name,
+        new Date(),
+        new Date()
+      );
+    });
+  }
+
+  /**
+   *
+   * @param id
+   * @param name
+   */
+  async update(id, name) {
+    return appDB.transaction(async () => {
+      // language=PostgreSQL
+      return await appDB.execute(
+        `
+          update his_department
+          set name       = ?,
+              updated_at = ?
+          where id = ?`,
+        name,
+        new Date(),
+        id
+      );
+    });
+  }
+
+  // 科室列表
+  async list() {
+    const hospital = await getHospital();
+    // language=PostgreSQL
+    return await appDB.execute(
+      `
+        select id, hospital, name, created_at
+        from his_department
+        where hospital = ?
+        order by created_at
+      `,
+      hospital
+    );
+  }
+}
