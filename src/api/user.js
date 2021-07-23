139415c631706212a4e6b01b7355430168d02f60
王贺辉 <wanghehui@bjknrt.com>
2021-07-23 09:49:31 +0800

    修改 新建用户接口

--- src/api/user.js
+++ src/api/user.js
@@ -261,36 +261,6 @@ export default class User {
       //批量设置用户角色关系
       await UserRoleModel.bulkCreate(roleUser);
 
-      // 兼容老代码
-      newUser.regionId = user.areaCode;
-      const regionModel = await RegionModel.findOne({
-        where: {code: newUser.areaCode}
-      });
-      if (regionModel) {
-        newUser.regionId = newUser.areaCode;
-      } else {
-        const hospitalModel = await HospitalModel.findOne({
-          where: {id: newUser.areaCode}
-        });
-        if (hospitalModel) {
-          newUser.regionId = hospitalModel.regionId;
-          await UserHospitalModel.create({
-            hospitalId: hospitalModel.id,
-            userId: newUser.id
-          });
-        } else {
-          // 中心层, 既不是区划, 也不是机构
-          const hospitalRegions = await appDB.execute(
-            `select h.region from hospital_mapping hm inner join hospital h on hm.h_id = h.id where u_id = ?`,
-            newUser.areaCode
-          );
-          if (hospitalRegions.length === 1) {
-            newUser.regionId = hospitalRegions[0].region;
-          }
-        }
-      }
-      await newUser.save();
-
       return newUser;
     });
   }
