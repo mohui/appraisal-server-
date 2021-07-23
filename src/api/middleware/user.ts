049ace3da74698d4270df6655fd00da113e7c9b7
王贺辉 <wanghehui@bjknrt.com>
2021-07-23 14:23:42 +0800

    修改中间件

--- src/api/middleware/user.ts
+++ src/api/middleware/user.ts
@@ -1,49 +1,86 @@
 import {Context} from '../context';
-import {
-  HospitalModel,
-  RegionModel,
-  RoleModel,
-  UserModel
-} from '../../database/model';
-import {getLeaves} from '../group';
-import {Op} from 'sequelize';
+import {appDB, originalDB} from '../../app';
 
 export async function UserMiddleware(ctx: Context | any, next: Function) {
   try {
     const token = ctx.req.header('token');
     if (token) {
-      const user =
-        (
-          await UserModel.findOne({
-            where: {id: Context.req.headers.token},
-            include: [
-              {
-                model: HospitalModel,
-                through: {
-                  attributes: []
-                }
-              },
-              {
-                model: RoleModel,
-                through: {
-                  attributes: []
-                }
-              },
-              {
-                model: RegionModel
-              }
-            ]
-          })
-        )?.toJSON() ?? null;
-      if (!user) {
+      // 查询用户表
+      // language=PostgreSQL
+      const userModels = await appDB.execute(
+        `select "user".id,
+                    "user".account,
+                    "user".name,
+                    "user".password,
+                    "user".area "areaCode",
+                    "user".region "regionId",
+                    "user".creator "creatorId",
+                    "user".editor "editorId",
+                    "user".created_at,
+                    "user".updated_at
+             from "user"
+             where id = ?`,
+        token
+      );
+      if (userModels.length === 0) {
         await next();
         return;
       }
-      //该用户的默认code
+      const user = userModels[0];
+
+      // 根据用户权限id查询下属机构
+      user.hospitals = await originalDB.execute(
+        // language=PostgreSQL
+        `select code id,
+                  name,
+                  parent
+             from area
+             where label in ('hospital.center', 'hospital.station')
+               and (code = ? or path like ?)`,
+        user.areaCode,
+        `%${user.areaCode}%`
+      );
+
+      // 根据地区查询当前地区信息
+      // language=PostgreSQL
+      user.region =
+        (
+          await originalDB.execute(
+            `
+                  select code,
+                         name,
+                         parent,
+                         label
+                  from area
+                  where code = ?
+                `,
+            user.areaCode
+          )
+        )[0] ?? null;
+
+      // 该用户的默认code
       user.code = user.areaCode;
       //用户是否为地区权限
-      user.isRegion = user.code === user.regionId;
-      //用户权限去重整理
+      user.isRegion =
+        user.region.label === 'hospital.center' ||
+        user.region.label === 'hospital.station'
+          ? false
+          : true;
+
+      // 查询role相关
+      // language=PostgreSQL
+      user.roles = await appDB.execute(
+        `
+            select role.id,
+                   role.name,
+                   role.creator,
+                   role.permissions
+            from user_role_mapping mapping
+                   left join role on mapping.role_id = role.id
+            where mapping.user_id = ?
+          `,
+        token
+      );
       user.permissions = [
         ...new Set(
           user.roles
@@ -51,23 +88,6 @@ export async function UserMiddleware(ctx: Context | any, next: Function) {
             .reduce((result, next) => result.concat(next), [])
         )
       ];
-      //如果是地区级别的用户重新配置其所属的机构 TODO: 有点苟且
-      if (user.hospitals.length === 0) {
-        const children = await getLeaves(user.areaCode);
-        //查询该用户所属地区下的所有机构
-        user.hospitals = (
-          await HospitalModel.findAll({
-            where: {
-              id: {
-                [Op.in]: children
-                  .map(it => it.code)
-                  //TODO: 苟且区分一下地区和机构
-                  .filter(it => it.length === 36)
-              }
-            }
-          })
-        ).map(it => it.toJSON());
-      }
       ctx.user = user;
     }
   } catch (e) {
