6f8d08f8ffb3a31d4962c76cd4f5b047965f4f95
王贺辉 <wanghehui@bjknrt.com>
2021-07-26 13:33:33 +0800

    获取下级菜单

--- src/api/group/common.ts
+++ src/api/group/common.ts
@@ -16,6 +16,34 @@ export async function getHospitals(code: string) {
   );
 }
 
+export async function getChildrenArea(code: string) {
+  const children = await originalDB.execute(
+    // language=PostgreSQL
+    `select code,
+              name,
+              parent,
+              path,
+              label
+       from area
+       where parent = ?`,
+    code
+  );
+  if (children.length === 0) {
+    return await originalDB.execute(
+      // language=PostgreSQL
+      `select code,
+              name,
+              parent,
+              path,
+              label
+       from area
+       where code = ?`,
+      code
+    );
+  }
+  return children;
+}
+
 export default class Common {
   async test(code) {
     return getHospitals(code);
