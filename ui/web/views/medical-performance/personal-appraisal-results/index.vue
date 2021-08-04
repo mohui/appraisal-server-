d8dbf052483f5d4e4c9294ee1ebb9674d542a3d2
谢高军 <xiegaojun@bjknrt.com>
2021-08-04 16:21:37 +0800

    校正前/后工分明细表中删除类型列

--- ui/web/views/medical-performance/personal-appraisal-results/index.vue
+++ ui/web/views/medical-performance/personal-appraisal-results/index.vue
@@ -186,11 +186,6 @@
         :cell-style="{textAlign: 'center'}"
       >
         <el-table-column
-          property="type"
-          label="类型"
-          min-width="150"
-        ></el-table-column>
-        <el-table-column
           property="name"
           label="名称"
           min-width="200"
@@ -200,7 +195,7 @@
             dialogScoreType === 'after' ? 'afterCorrectionScore' : 'score'
           "
           label="得分"
-          width="150"
+          min-width="150"
         ></el-table-column>
       </el-table>
     </el-dialog>
