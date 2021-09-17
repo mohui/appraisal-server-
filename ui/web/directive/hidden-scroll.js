dee03d35e5fa4934687aabda281041419e20e1b4
吴成浩 <wuchenghao@bjknrt.com>
2021-09-17 13:53:41 +0800

    滚动条指令解绑时清理一些事件

--- ui/web/directive/hidden-scroll.js
+++ ui/web/directive/hidden-scroll.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 const tableBodyClass = 'el-table__body-wrapper';
+let targetComp = null;
+let timer = null;
 
 Vue.directive('hidden-scroll', {
   inserted(el, binding, vnode) {
-    let targetComp = null;
     //默认获取element-table的body元素,因为项目中table滚动条居多
     targetComp = vnode.elm.parentElement.getElementsByClassName(
       tableBodyClass
@@ -14,25 +15,35 @@ Vue.directive('hidden-scroll', {
     if (targetComp) {
       //没滚动时也监测一下滚动条
       scrollHandler(targetComp);
-      targetComp.addEventListener('scroll', () => scrollHandler(targetComp));
+      targetComp.addEventListener('scroll', scrollHandler);
+    }
+  },
+  unbind() {
+    if (targetComp) {
+      //清理定时器
+      clearTimeout(timer);
+      //解除监听事件
+      targetComp.removeEventListener('scroll', scrollHandler);
     }
   }
 });
-let timer = null;
-function scrollHandler(el) {
-  //显示滚动条
-  el.classList.remove('hidden-scroll-bar');
-  //滚动条开始的位置
-  const start = el.scrollTop;
-  //清理定时器
-  clearTimeout(timer);
-  //隔一秒检查一下是否还在滚动
-  timer = setTimeout(() => {
-    const end = el.scrollTop;
-    if (end === start) {
-      //添加隐藏滚动条的样式
-      el.classList.add('hidden-scroll-bar');
-    }
-  }, 1000);
+
+function scrollHandler() {
+  if (targetComp) {
+    //显示滚动条
+    targetComp.classList.remove('hidden-scroll-bar');
+    //滚动条开始的位置
+    const start = targetComp.scrollTop;
+    //清理定时器
+    clearTimeout(timer);
+    //隔一秒检查一下是否还在滚动
+    timer = setTimeout(() => {
+      const end = targetComp.scrollTop;
+      if (end === start) {
+        //添加隐藏滚动条的样式
+        targetComp.classList.add('hidden-scroll-bar');
+      }
+    }, 1000);
+  }
 }
 export default Vue.directive('hidden-scroll');
