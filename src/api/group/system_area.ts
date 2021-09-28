abf0f949ad2e4c0ed78a47560c0f3934f983ae95
王贺辉 <wanghehui@bjknrt.com>
2021-09-28 09:48:49 +0800

    修复bug

--- src/api/group/system_area.ts
+++ src/api/group/system_area.ts
@@ -711,7 +711,8 @@ export default class SystemArea {
       // 先把所有的上级放到机构数组中
       const newHospitals = hospitals.map(it => {
         const index = childrenTree.find(child =>
-          'hospital.center' || child.leabel === 'hospital.station'
+          child.label === 'hospital.center' ||
+          child.label === 'hospital.station'
             ? it.path.includes(`.${child.code}`)
             : it.path.includes(`.${child.code}.`)
         );
