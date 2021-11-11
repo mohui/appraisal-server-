180ace65f69b6e272921e8fb0d4e3f0a98cf1b7d
王贺辉 <wanghehui@bjknrt.com>
2021-11-11 16:31:12 +0800

    修改接口 ratioOfMedicalAndNursing 兼容机构以上权限

--- src/api/app/home.ts
+++ src/api/app/home.ts
@@ -234,15 +234,9 @@ export default class AppHome {
 
     // 获取所属地区
     const group = Context.current.user.areaCode;
-    // 获取权限下机构
-    const areaModels = await getHospitals(group);
-    if (areaModels.length > 1) throw new KatoRuntimeError(`不是机构权限`);
-
-    // 取出机构id
-    const hospital = areaModels[0]?.code;
 
     // 取出机构下所有医生信息
-    const staffs = await getStaffList(hospital, date);
+    const staffs = await getStaffList(group, date);
 
     return divisionOperation(staffs.physicianCount, staffs.nurseCount);
   }
