3d75ea781547532ae0d4b0b9de6e0ec6f170f84e
吴成浩 <wuchenghao@bjknrt.com>
2021-12-02 16:47:53 +0800

    根据权限来动态配置路由

--- ui/web/plugins/setting.js
+++ ui/web/plugins/setting.js
@@ -1,3 +1,6 @@
+import {UserType} from '../../../common/user.ts';
+import {Permission} from '../../../common/permission.ts';
+
 export const settingPlugin = {
   install: function(Vue) {
     Vue.prototype.$settings = new Vue({
@@ -13,6 +16,13 @@ export const settingPlugin = {
           this.user = await this.$api.User.profile();
           //TODO: 暂时处理员工类型的用户没有权限和角色字段
           this.user.permissions = this.user.permissions || [];
+          if (this.user.type === UserType.STAFF) {
+            this.user.permissions.push(
+              Permission.APPRAISAL_RESULT,
+              Permission.MEDICAL_WORK
+            );
+            this.permissions = this.user.permissions;
+          }
           if (this.user && this.user.roles)
             this.permissions = [
               ...new Set(this.user.roles.map(it => it.permissions).flat())
