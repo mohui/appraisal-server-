243ae13f83e633d9478b4f5086e6f0ca8d5569dc
吴成浩 <wuchenghao@bjknrt.com>
2021-12-09 09:27:24 +0800

    公卫员工列表接口补充his字段

--- src/api/his/staff.ts
+++ src/api/his/staff.ts
@@ -540,6 +540,7 @@ export default class HisStaff {
       const phStaffIndex = sysUserList.find(item => it.phStaff === item.id);
       return {
         ...it,
+        his: index?.id ?? '',
         staffName: index?.name ?? '',
         departmentName: deptIndex?.name ?? '',
         phStaffName: phStaffIndex?.username ?? ''
