175594aa8a11771639f77e602f28f5b374d8655f
陈秀瑶 <chenxiuyao@bjknrt.com>
2021-12-10 17:13:49 +0800

    提交新的单元测试方法

--- __tests__/common/his.ts
+++ __tests__/common/his.ts
@@ -1,102 +1,93 @@
 import {multistep} from '../../common/his';
+import Decimal from 'decimal.js';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function equalResult(
+  rules: {start: number | null; end: number | null; unit: number}[],
+  exceptResult: {num: number; result: number[]}[]
+) {
+  for (const except of exceptResult) {
+    const result = multistep(rules, except.num);
+    //判断
+    expect(result).toStrictEqual(
+      rules.map((rule, index) => {
+        return {
+          ...rule,
+          num: except.result[index] ?? 0,
+          total: Decimal.mul(except.result[index] ?? 0, rule.unit).toNumber()
+        };
+      })
+    );
+  }
+}
 
 describe('梯度计算', () => {
   test('通用梯度', async () => {
     //梯度规则
-    const rules = [
-      {
-        start: null,
-        end: null,
-        unit: 10
-      }
+    const rules = [{start: null, end: null, unit: 10}];
+    const exceptResult = [
+      {num: -100, result: [-100]},
+      {num: 0, result: [0]},
+      {num: 100, result: [100]}
     ];
-    //总量
-    const num = 100;
-    //预计总量
-    const exceptResult = rules.map(it => ({
-      ...it,
-      num: 100,
-      total: 1000
-    }));
-    //梯度计算
-    const result = multistep(rules, num);
-    //判断
-    expect(result).toStrictEqual(exceptResult);
+    equalResult(rules, exceptResult);
   });
   test('正向梯度:整数区间', async () => {
     //梯度规则
     const rules = [
-      {
-        start: null,
-        end: 10,
-        unit: 1
-      },
-      {
-        start: 10,
-        end: null,
-        unit: 2
-      }
+      {start: null, end: 10, unit: 1},
+      {start: 10, end: 20, unit: 2},
+      {start: 20, end: null, unit: 3}
     ];
-    //总量
-    const num = 100;
-    //预计总量
     const exceptResult = [
-      {
-        start: null,
-        end: 10,
-        unit: 1,
-        num: 10,
-        total: 10
-      },
-      {
-        start: 10,
-        end: null,
-        unit: 2,
-        num: 90,
-        total: 180
-      }
+      {num: 0, result: [0]},
+      {num: 5, result: [5]},
+      {num: 10, result: [10]},
+      {num: 15, result: [10, 5]},
+      {num: 20, result: [10, 10]},
+      {num: 25, result: [10, 10, 5]}
     ];
-    //梯度计算
-    const result = multistep(rules, num);
-    //判断
-    expect(result).toStrictEqual(exceptResult);
+    equalResult(rules, exceptResult);
   });
   test('正向梯度:小数区间', async () => {
     //梯度规则
     const rules = [
-      {
-        start: null,
-        end: 10.1,
-        unit: 1
-      },
-      {
-        start: 10.1,
-        end: null,
-        unit: 2
-      }
+      {start: null, end: 10.1, unit: 1},
+      {start: 10.1, end: 20, unit: 2},
+      {start: 20, end: 30.1, unit: 3},
+      {start: 30.1, end: null, unit: 4}
     ];
-    //总量
-    const num = 100;
-    //预计总量
     const exceptResult = [
-      {
-        start: null,
-        end: 10.1,
-        unit: 1,
-        num: 10.1,
-        total: 10.1
-      },
-      {
-        start: 10.1,
-        end: null,
-        unit: 2,
-        num: 89.9,
-        total: 179.8
-      }
+      {num: 0, result: [0]},
+      {num: 5, result: [5]},
+      {num: 10.1, result: [10.1]},
+      {num: 15, result: [10.1, 4.9]},
+      {num: 20, result: [10.1, 9.9]},
+      {num: 25, result: [10.1, 9.9, 5]},
+      {num: 30.1, result: [10.1, 9.9, 10.1]},
+      {num: 35, result: [10.1, 9.9, 10.1, 4.9]}
     ];
-    //梯度计算
-    const result = multistep(rules, num);
-    //判断
-    expect(result).toStrictEqual(exceptResult);
+    equalResult(rules, exceptResult);
+  });
+  test('负向梯度(整数/小数)', async () => {
+    //梯度规则
+    const rules = [
+      {start: null, end: -30.1, unit: 4},
+      {start: -30.1, end: -20, unit: 3},
+      {start: -20, end: -10.1, unit: 2},
+      {start: -10.1, end: null, unit: 1}
+    ];
+    const exceptResult = [
+      {num: -31, result: [-0.9, -10.1, -9.9, -10.1]},
+      {num: -30.1, result: [0, -10.1, -9.9, -10.1]},
+      {num: -25, result: [0, -5, -9.9, -10.1]},
+      {num: -20, result: [0, 0, -9.9, -10.1]},
+      {num: -15, result: [0, 0, -4.9, -10.1]},
+      {num: -10.1, result: [0, 0, 0, -10.1]},
+      {num: -10, result: [0, 0, 0, -10]},
+      {num: 0, result: []},
+      {num: 1, result: [0, 0, 0, 1]}
+    ];
+    equalResult(rules, exceptResult);
   });
 });
