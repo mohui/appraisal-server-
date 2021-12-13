4e06230ca9ca2fd4b3760cc0e980b739793958a8
陈秀瑶 <chenxiuyao@bjknrt.com>
2021-12-13 15:04:32 +0800

    补充梯度测试的校验

--- __tests__/common/his.ts
+++ __tests__/common/his.ts
@@ -1,5 +1,6 @@
 import {multistep} from '../../common/his';
 import Decimal from 'decimal.js';
+import {KatoCommonError} from 'kato-server';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function equalResult(
@@ -8,6 +9,10 @@ function equalResult(
 ) {
   for (const except of exceptResult) {
     const result = multistep(rules, except.num);
+    if (
+      except.num !== result.reduce((result, current) => result + current.num, 0)
+    )
+      throw new KatoCommonError(`数据总量不符：${except}`);
     //判断
     expect(result).toStrictEqual(
       rules.map((rule, index) => {
@@ -87,6 +92,30 @@ describe('梯度计算', () => {
       {num: -10, result: [0, 0, 0, -10]},
       {num: 0, result: []},
       {num: 1, result: [0, 0, 0, 1]}
+    ];
+    equalResult(rules, exceptResult);
+  });
+  test('全梯度计算(负整数/负小数/0/正整数/正小数)', async () => {
+    //梯度规则
+    const rules = [
+      {start: null, end: -10, unit: 10},
+      {start: -10, end: -7.5, unit: 9},
+      {start: -7.5, end: -5, unit: 8},
+      {start: -5, end: 2.5, unit: 7},
+      {start: 2.5, end: 10, unit: 6},
+      {start: 10, end: 17.5, unit: 5},
+      {start: 17.5, end: null, unit: 4}
+    ];
+    const exceptResult = [
+      {num: -5, result: [0, 0, 0, -5]}, //负数1个梯度
+      {num: -6, result: [0, 0, -1, -5]}, //负数2个梯度
+      {num: -2.5, result: [0, 0, 0, -2.5]}, //负小数1个梯度
+      {num: -6.5, result: [0, 0, -1.5, -5]}, //负小数2个梯度
+      {num: 0, result: []}, //0
+      {num: 2, result: [0, 0, 0, 2]}, //正数1个梯度
+      {num: 5, result: [0, 0, 0, 2.5, 2.5]}, //正数2个梯度
+      {num: 1.5, result: [0, 0, 0, 1.5]}, //小数1个梯度
+      {num: 5.5, result: [0, 0, 0, 2.5, 3]} //小数2个梯度
     ];
     equalResult(rules, exceptResult);
   });
