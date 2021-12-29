e9bd2fd5000de5307d6827e9ff5c6e3ca9d9b710
戴凯杰 <daikaijie@bjknrt.com>
2021-12-29 16:27:28 +0800

    添加 智慧公卫 相关接口原型

--- src/api/app/area.ts
+++ src/api/app/area.ts
@@ -13,4 +13,60 @@ export default class AppArea {
   async joinUs(ticket) {
     return '';
   }
+
+  /**
+   * 机构概览
+   *
+   * @param area 机构编码
+   * @return {
+   *   name: 名称
+   *   workpoints?: 参与校正工分
+   *   rate?: 质量系数
+   *   date: 更新时间
+   *   people: [{
+   *     id: 人群分类编码
+   *     name: 人群名称
+   *     amount: 不规范人数
+   *   }]
+   * }
+   */
+  overview(area) {
+    return {};
+  }
+
+  /**
+   * 考核指标得分列表
+   *
+   * 目前只考虑机构
+   * @param area 地区编码
+   * @return 考核体系下的指标得分列表[]
+   */
+  async indicators(area) {
+    return [];
+  }
+
+  /**
+   * 档案问题标签列表
+   */
+  async tags() {
+    return [];
+  }
+
+  /**
+   * 问题档案列表
+   *
+   * 目前只考虑机构
+   * @param params {
+   *   area: 地区编码
+   *   keyword: 姓名/身份证
+   *   doctor: 录入医生
+   *   tags: []档案问题
+   *   pageSize: 分页大小
+   *   pageNo: 分页页码
+   * }
+   * @return 居民档案列表[]
+   */
+  async archives(params) {
+    return [];
+  }
 }
