a12ecbf5acd11d8afd2737800eea71e8af8de6ea
戴凯杰 <daikaijie@bjknrt.com>
2021-12-30 10:22:42 +0800

    调整员工申请表

--- src/database/migrations/052_add_staff_request.ts
+++ src/database/migrations/052_add_staff_request.ts
@@ -2,7 +2,7 @@ import {IMigration} from '../migrater';
 import {ExtendedSequelize} from '../client';
 
 export class AddStaffRequest implements IMigration {
-  name = '添加员工绑定申请表';
+  name = '添加员工申请表';
   version = 52;
 
   async up(client: ExtendedSequelize): Promise<void> {
@@ -12,16 +12,15 @@ export class AddStaffRequest implements IMigration {
       (
         id         char(36) primary key,
         staff      char(36),
-        hospital   char(36),
+        area       char(36),
         status     varchar(255)             not null default '待审核',
         created_at timestamp with time zone not null default current_timestamp,
-        updated_at timestamp with time zone not null default current_timestamp,
-        unique (staff, hospital)
+        updated_at timestamp with time zone not null default current_timestamp
       );
-      comment on table staff_request is '员工绑定申请表';
+      comment on table staff_request is '员工申请表';
       comment on column staff_request.id is '主键';
       comment on column staff_request.staff is '员工id';
-      comment on column staff_request.hospital is '机构id';
+      comment on column staff_request.area is '机构id';
       comment on column staff_request.status is '状态. 待审核;已通过;未通过';
     `);
   }
