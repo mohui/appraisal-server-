ffc92f58db531cfb48431f5a8632400f3d92fda5
王贺辉 <wanghehui@bjknrt.com>
2022-01-28 16:30:14 +0800

    去掉model的用法

--- __tests__/middleware/user.ts
+++ __tests__/middleware/user.ts
@@ -1,13 +1,6 @@
 import {app, appDB} from '../../src/app';
 import {KatoClient} from 'kato-client';
-import {
-  HospitalModel,
-  RegionModel,
-  RoleModel,
-  UserModel
-} from '../../src/database';
 import {getHospitals} from '../../src/api/group/common';
-import {Op} from 'sequelize';
 
 let api;
 beforeAll(async () => {
@@ -26,32 +19,79 @@ afterAll(async () => {
  * @param token token
  */
 async function getUser(token) {
-  const user =
-    (
-      await UserModel.findOne({
-        where: {id: token},
-        include: [
-          {
-            model: HospitalModel,
-            through: {
-              attributes: []
-            }
-          },
-          {
-            model: RoleModel,
-            through: {
-              attributes: []
-            }
-          },
-          {
-            model: RegionModel
-          }
-        ]
-      })
-    )?.toJSON() ?? null;
+  const user = (
+    await appDB.execute(
+      // language=PostgreSQL
+      `
+        select "id",
+               "account",
+               "name",
+               "password",
+               "area"    AS "areaCode",
+               "region"  AS "regionId",
+               "creator" AS "creatorId",
+               "editor"  AS "editorId",
+               "created_at",
+               "updated_at"
+        FROM "user"
+        WHERE id = ?
+      `,
+      token
+    )
+  )[0];
   if (!user) {
     return user;
   }
+  //如果是地区级别的用户重新配置其所属的机构 TODO: 有点苟且
+  const children = await getHospitals(user.areaCode);
+  const hospitalIds = children
+    .map(it => it.code)
+    //TODO: 苟且区分一下地区和机构
+    .filter(it => it.length === 36);
+  //查询该用户所属地区下的所有机构
+  user.hospitals = (
+    await appDB.execute(
+      // language=PostgreSQL
+      `
+            SELECT "id",
+                   "name",
+                   "parent",
+                   "region" AS "regionId",
+                   "created_at",
+                   "updated_at"
+            FROM "hospital" AS "Hospital"
+            WHERE "Hospital"."id" IN (${hospitalIds.map(() => '?')})
+          `,
+      ...hospitalIds
+    )
+  ).map(it => ({
+    his: it.regionId.startsWith('340222') ? '340222' : '340203',
+    ...it
+  }));
+  // 用户权限
+  user.roles = await appDB.execute(
+    // language=PostgreSQL
+    `
+      select role.id, role.name, role.creator, role.permissions, role.created_at, role.updated_at
+      from role
+             inner join user_role_mapping mapping on role.id = mapping.role_id
+      where mapping.user_id = ?
+    `,
+    token
+  );
+  user.region =
+    (
+      await appDB.execute(
+        // language=PostgreSQL
+        `
+          select code, name, level, parent, budget, created_at, updated_at
+          from region
+          where code = ?
+        `,
+        user.regionId
+      )
+    )[0] ?? null;
+
   //该用户的默认code
   user.code = user.areaCode;
   //用户是否为地区权限
@@ -64,23 +104,6 @@ async function getUser(token) {
         .reduce((result, next) => result.concat(next), [])
     )
   ];
-  //如果是地区级别的用户重新配置其所属的机构 TODO: 有点苟且
-  if (user.hospitals.length === 0) {
-    const children = await getHospitals(user.areaCode);
-    //查询该用户所属地区下的所有机构
-    user.hospitals = (
-      await HospitalModel.findAll({
-        where: {
-          id: {
-            [Op.in]: children
-              .map(it => it.code)
-              //TODO: 苟且区分一下地区和机构
-              .filter(it => it.length === 36)
-          }
-        }
-      })
-    ).map(it => it.toJSON());
-  }
   return user;
 }
 
