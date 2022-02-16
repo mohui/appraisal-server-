a6720bea6cf68a58832a62c01f6c93fc8c48ea0e
戴凯杰 <daikaijie@bjknrt.com>
2022-02-16 17:05:09 +0800

    快速复制紧急添加2022年选项

--- ui/web/views/check/index.vue
+++ ui/web/views/check/index.vue
@@ -269,6 +269,7 @@
           <el-select v-model="copyForm.checkYear">
             <el-option :value="2020">2020</el-option>
             <el-option :value="2021">2021</el-option>
+            <el-option :value="2022">2022</el-option>
           </el-select>
         </el-form-item>
       </el-form>
