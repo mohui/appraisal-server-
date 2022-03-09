c4d722c417579d108e02fdd089be39be0fd592ef
戴凯杰 <daikaijie@bjknrt.com>
2022-03-09 03:19:34 +0000

    Revert "Merge branch 'feature/temporary-staff' into 'feature/app'"
    This reverts merge request !904

--- src/api/his/staff.ts
+++ src/api/his/staff.ts
@@ -50,22 +50,13 @@ export default class HisStaff {
         phStaffs: should.array(),
         hisStaffs: should.array(),
         department: should.string().allow(null),
-        remark: should.string().allow(null),
-        phone: should.string().required()
+        remark: should.string().allow(null)
       })
       .required()
   )
   async updateStaffMapping(params) {
     // 取出所有的变量
-    const {
-      id,
-      hospital,
-      phStaffs,
-      hisStaffs,
-      department,
-      remark,
-      phone
-    } = params;
+    const {id, hospital, phStaffs, hisStaffs, department, remark} = params;
     // 机构下的所有公卫员工
     const phStaffModels = await originalDB.execute(
       // language=PostgreSQL
@@ -173,31 +164,6 @@ export default class HisStaff {
         dayjs().toDate(),
         id,
         hospital
-      );
-      // TODO:临时使用,过后要删除的
-      // 校验手机号是否重复
-      const phoneSel = await appDB.execute(
-        // language=PostgreSQL
-        `
-          select *
-          from staff
-          where phone = ?
-            and id != ?
-        `,
-        phone,
-        id
-      );
-      if (phoneSel.length > 0) throw new KatoRuntimeError(`手机号已经存在`);
-      await appDB.execute(
-        // language=PostgreSQL
-        `
-          update staff
-          set phone      = ?,
-              updated_at = ?
-          where id = ?`,
-        phone,
-        dayjs().toDate(),
-        id
       );
     });
   }
@@ -993,12 +959,12 @@ export default class HisStaff {
       return await appDB.execute(
         // language=PostgreSQL
         `
-          update staff
-          set hospital   = null,
-              department = null,
-              updated_at = ?
-          where id = ?
-        `,
+              update staff
+              set hospital   = null,
+                  department = null,
+                  updated_at = ?
+              where id = ?
+            `,
         dayjs().toDate(),
         id
       );
