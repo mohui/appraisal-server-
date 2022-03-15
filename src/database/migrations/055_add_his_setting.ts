293746cc65640f1cea1cfa58785a179abe290743
王贺辉 <wanghehui@bjknrt.com>
2022-03-15 14:22:34 +0800

    修改表备注和字段

--- src/database/migrations/055_add_his_setting.ts
+++ src/database/migrations/055_add_his_setting.ts
@@ -2,7 +2,7 @@ import {IMigration} from '../migrater';
 import {ExtendedSequelize} from '../client';
 
 export class AddHisSetting implements IMigration {
-  name = '添加智慧医疗公分项管理表';
+  name = '添加医疗绩效设置表';
   version = 55;
 
   async up(client: ExtendedSequelize): Promise<void> {
@@ -12,15 +12,15 @@ export class AddHisSetting implements IMigration {
       (
         hospital     varchar(36),
         code         varchar(255),
-        settle       boolean                  not null DEFAULT true,
+        enabled      boolean                  not null DEFAULT true,
         "created_at" timestamp with time zone not null default current_timestamp,
         "updated_at" timestamp with time zone not null default current_timestamp,
         primary key (hospital, code)
       );
-      comment on table his_setting is '智慧医疗公分项管理表';
+      comment on table his_setting is '医疗绩效设置表';
       comment on column his_setting.hospital is '所属机构id';
       comment on column his_setting.code is '功能编码';
-      comment on column his_setting.settle is '是否开启';
+      comment on column his_setting.enabled is '是否开启';
     `);
   }
 
