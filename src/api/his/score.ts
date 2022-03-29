7c3bcd8281d6c6b6178242d2b6f13190d90b2f53
王贺辉 <wanghehui@bjknrt.com>
2022-03-29 10:53:07 +0800

    优化排序代码

--- src/api/his/score.ts
+++ src/api/his/score.ts
@@ -557,17 +557,12 @@ export async function workPointCalculation(
   //endregion
 
   return workItems.sort((a, b) => {
-    if (a.date.getTime() === b.date.getTime() && a.itemId === b.itemId) {
-      return 1;
+    const dateDiff = b.date.getTime() - a.date.getTime();
+    if (dateDiff === 0) {
+      return b.itemId > a.itemId ? 1 : -1;
+    } else {
+      return dateDiff;
     }
-    const names = [a.itemId, b.itemId].sort();
-    if (
-      a.date.getTime() > b.date.getTime() ||
-      (a.date.getTime() === b.date.getTime() && b.itemId === names[0])
-    ) {
-      return -1;
-    }
-    return 1;
   });
 }
 
