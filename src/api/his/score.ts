7dcacbdf8130c5d0e1f63b0c74898283079afd79
王贺辉 <wanghehui@bjknrt.com>
2022-03-29 10:00:41 +0800

    根据时间和工分来源id倒序排序

--- src/api/his/score.ts
+++ src/api/his/score.ts
@@ -555,9 +555,20 @@ export async function workPointCalculation(
     );
   }
   //endregion
-  return workItems.sort((a, b) =>
-    a.date.getTime() < b.date.getTime() ? 1 : -1
-  );
+
+  return workItems.sort((a, b) => {
+    if (a.date.getTime() === b.date.getTime() && a.itemId === b.itemId) {
+      return 1;
+    }
+    const names = [a.itemId, b.itemId].sort();
+    if (
+      a.date.getTime() > b.date.getTime() ||
+      (a.date.getTime() === b.date.getTime() && b.itemId === names[0])
+    ) {
+      return -1;
+    }
+    return 1;
+  });
 }
 
 // endregion
