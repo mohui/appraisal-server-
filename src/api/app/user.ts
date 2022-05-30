0e9161817a5b6f5a9c5a20931585a0388c8ce0ed
王贺辉 <wanghehui@bjknrt.com>
2022-05-30 17:34:46 +0800

    删除''

--- src/api/app/user.ts
+++ src/api/app/user.ts
@@ -904,9 +904,9 @@ export default class AppUser {
    * APP机构列表
    *
    * @return [{
-   *  id: '机构id',
-   *  name: '机构名称',
-   *  status: '状态: 已通过,未通过,审核中',
+   *  id: 机构id,
+   *  name: 机构名称,
+   *  status: 状态: 已通过,未通过,审核中,
    *  primary: 是否是主机构: true,false
    * }]
    */
