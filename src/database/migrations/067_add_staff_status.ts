91fde054f04387ad66606433dd7b48cac3cd30c0
王贺辉 <wanghehui@bjknrt.com>
2022-06-14 16:43:20 +0800

    修改默认值

--- src/database/migrations/067_add_staff_status.ts
+++ src/database/migrations/067_add_staff_status.ts
@@ -2,15 +2,15 @@ import {IMigration} from '../migrater';
 import {ExtendedSequelize} from '../client';
 
 export class AddStaffStatus implements IMigration {
-  name = '员工表添加是否注销字段';
+  name = '员工表添加是否可用字段';
   version = 67;
 
   async up(client: ExtendedSequelize): Promise<void> {
     // language=PostgreSQL
     await client.execute(`
       ALTER table staff
-        ADD COLUMN status boolean not null default false;
-      COMMENT ON COLUMN staff.status IS '是否注销,默认false';
+        ADD COLUMN status boolean not null default true;
+      COMMENT ON COLUMN staff.status IS '是否可用,默认true';
     `);
   }
 
