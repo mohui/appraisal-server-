5291ab17e3029e7a49b6b2d1803308e1b15c0aa2
王贺辉 <wanghehui@bjknrt.com>
2022-06-15 10:26:20 +0800

    修改hisStaff.checkStaff添加过滤员工状态是false的条件

--- src/api/his/check.ts
+++ src/api/his/check.ts
@@ -722,8 +722,10 @@ export default class HisCheck {
         from staff
                inner join staff_area_mapping areaMapping on staff.id = areaMapping.staff
         where areaMapping.area = ?
+          and staff.status = ?
       `,
-      hospital
+      hospital,
+      true
     );
     // 未绑定过考核的员工
     const [sql, params] = sqlRender(
